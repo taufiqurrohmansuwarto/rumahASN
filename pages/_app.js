bfcd2a91450aba29b07f859ce194f9d2fbe63e9a
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-09-27 11:27:10 +0700

    last commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,40 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { useState } from "react";
+import { SessionProvider } from "next-auth/react";
+import { ConfigProvider } from "antd";
+import id from "antd/lib/locale/id_ID";
+
+function Auth({ children, roles }) {
+  return <>{children}</>;
+}
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  const [queryClient] = useState(() => new QueryClient());
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <SessionProvider
+      session={session}
+      baseUrl="/helpdesk"
+      basePath="/helpdesk/api/auth"
+    >
+      <QueryClientProvider client={queryClient}>
+        <ConfigProvider locale={id}>
+          <Hydrate>
+            {Component.Auth ? (
+              <Auth>{getLayout(<Component {...pageProps} />)}</Auth>
+            ) : (
+              <Component {...pageProps} />
+            )}
+          </Hydrate>
+        </ConfigProvider>
+      </QueryClientProvider>
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
