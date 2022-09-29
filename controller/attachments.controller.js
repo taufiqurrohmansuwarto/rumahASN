8d04a0c5e2b687fa0a7d202dd152e53d60b992ff
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-09-29 11:00:16 +0700

    last commit

---
+++ controller/attachments.controller.js
@@ -0,0 +1,4 @@
+module.exports.index = async (req, res) => {};
+module.exports.remove = async (req, res) => {};
+module.exports.update = async (req, res) => {};
+module.exports.create = async (req, res) => {};
