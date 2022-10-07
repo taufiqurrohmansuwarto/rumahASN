5da8046fdf66db39ca4fc0f447bd68da0d9b87ac
Iput <taufiqurrohman.suwarto@gmail.com>
2022-10-07 13:53:17 +0700

    last commit

--- utils/ability.js
+++ utils/ability.js
@@ -1,6 +1,7 @@
 import { defineAbility } from "@casl/ability";
 
-const ability = (role) => {
+const ability = (user) => {
+  const { role } = user;
   return defineAbility((can, cannot) => {
     // there is 3 roles: admin, agent, user
     if (role === "admin") {
@@ -9,8 +10,13 @@ const ability = (role) => {
       can("manage", "Tickets");
       can("manage", "Feeds");
     } else if (role === "user") {
+      can(
+        "manage",
+        "Tickets",
+        { user_id: req.user.userId },
+        { status: "open" }
+      );
       can("manage", "Feeds");
-      can("manage", "Tickets");
     }
   });
 };
