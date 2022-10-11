481fb6b3896756984d33d9b08fb13761190851e6
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-10-11 10:34:46 +0700

    users management

--- pages/admin/users-management.js
+++ pages/admin/users-management.js
@@ -1,13 +1,63 @@
 import { useQuery } from "@tanstack/react-query";
+import { Avatar, Button, Card, Table } from "antd";
+import { useState } from "react";
 import { getUsers } from "../../services";
 import AdminLayout from "../../src/components/AdminLayout";
 import PageContainer from "../../src/components/PageContainer";
 
+const columns = [
+  {
+    title: "Foto",
+    key: "foto",
+    render: (text, record) => {
+      return <Avatar alt="foto profil" src={record?.image} />;
+    },
+  },
+  {
+    title: "Username",
+    dataIndex: "username",
+    key: "username",
+  },
+  {
+    title: "NIP",
+    dataIndex: "employee_number",
+    key: "employee_number",
+  },
+  {
+    title: "Role",
+    dataIndex: "current_role",
+    key: "current_role",
+  },
+  {
+    title: "Aksi",
+    key: "aksi",
+    render: (text, record) => <Button>Rubah</Button>,
+  },
+];
+
 const Dashboard = () => {
   const { data, isLoading } = useQuery(["users"], () => getUsers());
+  const [query, setQuery] = useState({
+    page: 1,
+    limit: 10,
+    search: "",
+    sort: "username",
+    order: "asc",
+  });
+
   return (
     <PageContainer>
       <div>{JSON.stringify(data)}</div>
+      <Card>
+        <Table
+          dataSource={data?.data}
+          columns={columns}
+          loading={isLoading}
+          pagination={{
+            total: data?.total,
+          }}
+        />
+      </Card>
     </PageContainer>
   );
 };
