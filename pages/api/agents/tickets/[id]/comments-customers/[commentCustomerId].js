f6853db93095a12835661ff7c1232bae33b60f50
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-10-26 12:24:35 +0700

    last commit

--- pages/api/agents/tickets/[id]/comments-customers/[commentCustomerId].js
+++ pages/api/agents/tickets/[id]/comments-customers/[commentCustomerId].js
@@ -1,8 +1,13 @@
 import { createRouter } from "next-connect";
 import auth from "../../../../../../middleware/auth.middleware";
+import {
+  detail,
+  update,
+  remove,
+} from "../../../../../../controller/tickets-comments-customers-to-agents.controller";
 
 const router = createRouter();
 
-router.use(auth).get().patch().delete();
+router.use(auth).get(detail).patch(update).delete(remove);
 
 export default router.handler();
--- pages/api/agents/tickets/[id]/comments-customers/index.js
+++ pages/api/agents/tickets/[id]/comments-customers/index.js
@@ -1,8 +1,12 @@
 import { createRouter } from "next-connect";
+import {
+  index,
+  create,
+} from "../../../../../../controller/tickets-comments-customers-to-agents.controller";
 import auth from "../../../../../../middleware/auth.middleware";
 
 const router = createRouter();
 
-router.use(auth).get().post();
+router.use(auth).get(index).post(create);
 
-export default router.handler();
+export default router.handler({});
