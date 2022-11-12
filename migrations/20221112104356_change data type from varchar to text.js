da86eb14fa72579a32bdca7a2948ae41f0170252
Iput <taufiqurrohman.suwarto@gmail.com>
2022-11-12 17:46:30 +0700

    last commit

---
+++ migrations/20221112104356_change data type from varchar to text.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.raw(
+    `alter table tickets_comments_customers alter column comment type text using comment::text;`
+  );
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.raw(
+    `alter table tickets_comments_customers alter column comment type varchar(255) using comment::varchar(255);`
+  );
+};
