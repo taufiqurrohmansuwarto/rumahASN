559ae49bef7e0048fdfd65e66d8527de185b600f
Iput <taufiqurrohman.suwarto@gmail.com>
2022-11-16 03:01:22 +0700

    change title

--- pages/signin.js
+++ pages/signin.js
@@ -15,7 +15,7 @@ export default function SignIn({ providers }) {
         backgroundImageUrl="https://siasn.bkd.jatimprov.go.id:9000/public/doodle-new.png"
         logo="https://siasn.bkd.jatimprov.go.id:9000/public/logobkd.jpg"
         title="Helpdesk BKD Jatim"
-        subTitle="MySAPK - SIASN"
+        subTitle="MySAPK - SIASN - SIMASTER"
         activityConfig={{
           style: {
             boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.2)",
