c8b3af43b04e2a219ec039e380c527fb299cd10b
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-03-20 16:45:14 +0700

    add last commit

--- src/components/TicketProps/ChangeFeedback.js
+++ src/components/TicketProps/ChangeFeedback.js
@@ -1,5 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 function ChangeFeedback() {
+  const { data, isloading } = useQuery(["changeFeedback"], () => {});
   return <div>ChangeFeedback</div>;
 }
 
