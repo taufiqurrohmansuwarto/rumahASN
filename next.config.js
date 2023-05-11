3a223345b7517814ba21c1cd9a2eb37e27abe79d
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-05-11 13:58:18 +0700

    last commit

--- next.config.js
+++ next.config.js
@@ -39,6 +39,37 @@ module.exports = withAntdLess({
   experimental: {
     scrollRestoration: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self' 'http://blog.logrocket.com'; image-src 'https://unsplash.com'; script-src 'self' https://www.google-analytics.com; font-src 'self' 'https://fonts.googleapis.com'",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(); battery=(self); geolocation=(); microphone=('https://a-domain.com')",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     domains: ["siasn.bkd.jatimprov.go.id"],
     formats: ["image/webp"],
