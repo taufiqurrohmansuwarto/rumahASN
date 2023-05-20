4b4bacbf689518cdce0d4a6d2ecf1580910f15e8
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-05-20 14:29:15 +0700

    add referensi menu referensi

--- pages/referensi/priorities.js
+++ pages/referensi/priorities.js
@@ -1,18 +1,253 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
 
+import { FileAddOutlined } from "@ant-design/icons";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  Button,
+  Card,
+  Divider,
+  Form,
+  Input,
+  message,
+  Modal,
+  Popconfirm,
+  Space,
+  Table,
+} from "antd";
+import { useEffect, useState } from "react";
+import {
+  createPriority,
+  deletePriority,
+  getPriorities,
+  updatePriority,
+} from "@/services/index";
+
+const { default: PageContainer } = require("@/components/PageContainer");
+
+const UpdateForm = ({ open, onCancel, data, update, loading }) => {
+  const [form] = Form.useForm();
+
+  const handleOk = async () => {
+    const value = await form.validateFields();
+    const patchData = {
+      id: data?.id,
+      data: { name: value.name, color: value.color },
+    };
+    update(patchData);
+  };
+
+  useEffect(() => {
+    form.setFieldsValue({
+      name: data?.name,
+      color: data?.color,
+    });
+  }, [form, data]);
+
+  return (
+    <Modal
+      centered
+      onOk={handleOk}
+      title="Update data"
+      confirmLoading={loading}
+      open={open}
+      onCancel={onCancel}
+    >
+      <Form form={form}>
+        <Form.Item name="name" label="Nama">
+          <Input />
+        </Form.Item>
+        <Form.Item name="color" label="Color">
+          <Input />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+const CreateForm = ({ open, onCancel }) => {
+  const [form] = Form.useForm();
+
+  const queryClient = useQueryClient();
+
+  const { mutate: create } = useMutation((data) => createPriority(data), {
+    onSettled: () => {
+      queryClient.invalidateQueries(["priorities"]);
+      onCancel();
+    },
+  });
+
+  const handleOk = async () => {
+    try {
+      const result = await form.validateFields();
+      create(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Modal
+      onOk={handleOk}
+      centered
+      title="Buat Prioritas"
+      destroyOnClose
+      open={open}
+      onCancel={onCancel}
+    >
+      <Form form={form}>
+        <Form.Item
+          rules={[{ required: true, message: "Nama tidak boleh kosogn" }]}
+          name="name"
+          label="Name"
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          rules={[{ required: true, message: "Warna tidak boleh kosong" }]}
+          name="color"
+          label="Warna"
+        >
+          <Input />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
 function ReferensiPrioritas() {
+  const { data, isLoading } = useQuery(["priorities"], () => getPriorities());
+
+  // wow fucking amazing
+  const [openCreate, setOpenCreate] = useState(false);
+  const [openUpdate, setOpenUpdate] = useState(false);
+  const [selectedData, setSelectedData] = useState(null);
+
+  const cancelCreate = () => setOpenCreate(false);
+  const cancelUpdate = () => setOpenUpdate(false);
+
+  const openCreateModal = () => {
+    setOpenCreate(true);
+  };
+
+  const openUpdateModal = (data) => {
+    setSelectedData(data);
+    setOpenUpdate(true);
+  };
+
+  const { mutate: update, isLoading: updateLoading } = useMutation(
+    (data) => updatePriority(data),
+    {
+      onSettled: () => {
+        queryClient.invalidateQueries(["priorities"]);
+      },
+      onSuccess: () => {
+        message.success("Berhasil mengupdate data");
+        cancelUpdate();
+      },
+      onError: (err) => {
+        message.error("Gagal mengupdate data");
+      },
+    }
+  );
+
+  const queryClient = useQueryClient();
+
+  const { mutate: hapus } = useMutation((id) => deletePriority(id), {
+    onSettled: () => {
+      queryClient.invalidateQueries(["priorities"]);
+      message.success("Berhasil menghapus data");
+    },
+    onError: (error) => {
+      message.error(JSON.stringify(error));
+    },
+  });
+
+  const handleHapus = (id) => {
+    hapus(id);
+  };
+
+  const columns = [
+    {
+      title: "Nama",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Warna",
+      dataIndex: "color",
+      key: "color",
+    },
+    {
+      title: "Dibuat pada",
+      dataIndex: "created_at",
+      key: "created_at",
+    },
+    {
+      title: "Oleh",
+      key: "created_by",
+      render: (text, record) => {
+        return record.createdBy?.username;
+      },
+    },
+    {
+      title: "Aksi",
+      key: "aksi",
+      render: (text, record) => {
+        return (
+          <Space>
+            <a onClick={() => openUpdateModal(record)}>Update</a>
+            <Divider type="vertical" />
+            <Popconfirm
+              onConfirm={() => handleHapus(record?.id)}
+              title="Apakah anda yakin ingin menghapus?"
+            >
+              <a>Hapus</a>
+            </Popconfirm>
+          </Space>
+        );
+      },
+    },
+  ];
+
   return (
     <>
       <Head>
         <title>Rumah ASN - Referensi - Priorities</title>
       </Head>
+      <PageContainer title="Kamus Referensi" subTitle="Daftar Prioritas">
+        <Card>
+          <Button
+            type="primary"
+            icon={<FileAddOutlined />}
+            onClick={openCreateModal}
+            style={{ marginBottom: 16 }}
+          >
+            Tambah
+          </Button>
+          <Table
+            columns={columns}
+            pagination={false}
+            dataSource={data}
+            rowKey={(row) => row?.id}
+            loading={isLoading}
+          />
+          <CreateForm open={openCreate} onCancel={cancelCreate} />
+          <UpdateForm
+            open={openUpdate}
+            data={selectedData}
+            update={update}
+            onCancel={cancelUpdate}
+            loading={updateLoading}
+          />
+        </Card>
+      </PageContainer>
     </>
   );
 }
 
 ReferensiPrioritas.getLayout = function getLayout(page) {
-  return <Layout active="/referensi/status">{page}</Layout>;
+  return <Layout active="/referensi/priorities">{page}</Layout>;
 };
 
 ReferensiPrioritas.Auth = {
--- pages/referensi/status.js
+++ pages/referensi/status.js
@@ -1,12 +1,218 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
 
+import { PlusOutlined } from "@ant-design/icons";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  Button,
+  Card,
+  Divider,
+  Form,
+  Input,
+  message,
+  Modal,
+  Popconfirm,
+  Space,
+  Table,
+} from "antd";
+import { useEffect, useState } from "react";
+import {
+  createStatus,
+  deleteStatus,
+  getStatus,
+  updateStatus,
+} from "@/services/index";
+
+const { default: PageContainer } = require("@/components/PageContainer");
+
+const CreateStatus = ({ open, onCancel }) => {
+  const [form] = Form.useForm();
+
+  const queryClient = useQueryClient();
+
+  const createMutation = useMutation((data) => createStatus(data), {
+    onSettled: () => {
+      queryClient.invalidateQueries(["status"]);
+    },
+    onSuccess: () => {
+      message.success("Berhasil membuat status");
+      onCancel();
+    },
+  });
+
+  const onFinish = async () => {
+    try {
+      const result = await form.validateFields();
+      createMutation.mutate(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Modal
+      destroyOnClose
+      onOk={onFinish}
+      centered
+      title="Buat Referensi Status"
+      open={open}
+      onCancel={onCancel}
+    >
+      <Form form={form}>
+        <Form.Item
+          label="Nama"
+          name="name"
+          rules={[{ required: true, message: "Nama tidak boleh kosong" }]}
+        >
+          <Input />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+const UpdateStatus = ({ open, onCancel, data }) => {
+  const [form] = Form.useForm();
+
+  const queryClient = useQueryClient();
+
+  const { mutate: update, isLoading } = useMutation(
+    (data) => updateStatus(data),
+    {
+      onSettled: () => {
+        queryClient.invalidateQueries(["status"]);
+      },
+      onSuccess: () => {
+        message.success("Berhasil mengubah status");
+        onCancel();
+      },
+    }
+  );
+
+  const handleUpdate = async () => {
+    try {
+      const result = await form.validateFields();
+      const dataSend = { id: data?.id, data: { name: result?.name } };
+      update(dataSend);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    form.setFieldsValue({ name: data?.name });
+  }, [data, form]);
+
+  return (
+    <Modal
+      onOk={handleUpdate}
+      confirmLoading={isLoading}
+      title="Update Data"
+      open={open}
+      onCancel={onCancel}
+    >
+      <Form form={form} initialValues={data}>
+        <Form.Item name="name">
+          <Input />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
 function ReferensiStatus() {
+  const { data, isLoading } = useQuery(["status"], () => getStatus());
+  const [visibleCreate, setVisibleCreate] = useState(false);
+  const [visibleUpdate, setVisibileUpdate] = useState(false);
+  const [dataUpdate, setDataUpdate] = useState(null);
+
+  const client = useQueryClient();
+
+  const handleCreateOpen = () => {
+    setVisibleCreate(true);
+  };
+
+  const onCancelCreate = () => {
+    setVisibleCreate(false);
+  };
+
+  const onCancelUpdate = () => {
+    setVisibileUpdate(false);
+  };
+
+  const { mutate: hapus } = useMutation((id) => deleteStatus(id), {
+    onSettled: () => {
+      client.invalidateQueries(["status"]);
+    },
+    onSuccess: () => {
+      message.success("Berhasil menghapus status");
+    },
+  });
+
+  const handleHapus = (id) => {
+    hapus(id);
+  };
+
+  const columns = [
+    {
+      title: "Nama",
+      key: "name",
+      dataIndex: "name",
+    },
+    {
+      title: "Aksi",
+      key: "aksi",
+      render: (_, record) => (
+        <Space>
+          <a
+            onClick={() => {
+              setDataUpdate(record);
+              setVisibileUpdate(true);
+            }}
+          >
+            Ubah
+          </a>
+          <Divider type="vertical" />
+          <Popconfirm
+            title="Apakah anda yakin ingin menghapus"
+            onConfirm={() => handleHapus(record?.id)}
+          >
+            <a>Hapus</a>
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
+
   return (
     <>
       <Head>
         <title>Rumah ASN - Referensi - Status</title>
       </Head>
+      <PageContainer title="Kamus Referensi" subTitle="Status Pertanyaan">
+        <Card>
+          <Button
+            style={{ marginBottom: 16 }}
+            onClick={handleCreateOpen}
+            type="primary"
+            icon={<PlusOutlined />}
+          >
+            Buat
+          </Button>
+          <Table
+            loading={isLoading}
+            dataSource={data}
+            columns={columns}
+            rowKey={(row) => row?.id}
+            pagination={false}
+          />
+          <CreateStatus onCancel={onCancelCreate} open={visibleCreate} />
+          <UpdateStatus
+            open={visibleUpdate}
+            data={dataUpdate}
+            onCancel={onCancelUpdate}
+          />
+        </Card>
+      </PageContainer>
     </>
   );
 }
