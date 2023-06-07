ea92f0bd2073dc566abfb679b0ac74d1d00263ac
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-06-08 06:03:11 +0700

    last commit

--- controller/zoom.controller.js
+++ controller/zoom.controller.js
@@ -1,3 +1,32 @@
+const crypto = require("crypto");
+const KJUR = require("jsrsasign");
+
+const sdkKey = process.env.ZOOM_MEETING_SDK_KEY;
+const sdkSecret = process.env.ZOOM_MEETING_SDK_SECRET;
+
+const generateSignature = (meetingNumber, role) => {
+  const iat = Math.round(new Date().getTime() / 1000) - 30;
+  const exp = iat + 60 * 60 * 2;
+
+  const oHeader = { alg: "HS256", typ: "JWT" };
+
+  const oPayload = {
+    sdkKey,
+    mn: meetingNumber,
+    role: role,
+    iat: iat,
+    exp: exp,
+    appKey: sdkKey,
+    tokenExp: iat + 60 * 60 * 2,
+  };
+
+  const sHeader = JSON.stringify(oHeader);
+  const sPayload = JSON.stringify(oPayload);
+  const signature = KJUR.jws.JWS.sign("HS256", sHeader, sPayload, sdkSecret);
+
+  return signature;
+};
+
 module.exports.zoomIndex = async (req, res) => {
   try {
     const zoomFetcher = req.zoomFetcher;
