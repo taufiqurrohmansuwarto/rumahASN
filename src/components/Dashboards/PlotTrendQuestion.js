2fd1cedfa77ef70da9c2fc4954f2ccb9d4d6b000
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-06-11 05:45:33 +0700

    last commit

--- src/components/Dashboards/PlotTrendQuestion.js
+++ src/components/Dashboards/PlotTrendQuestion.js
@@ -1,5 +1,4 @@
 import { trends } from "@/services/admin.services";
-import { Bar } from "@ant-design/plots";
 import { useQuery } from "@tanstack/react-query";
 import { Button, Card } from "antd";
 import { useRef } from "react";
@@ -40,7 +39,7 @@ function PlotTrendQuestion() {
       title="Trend Pertanyaan 3 Bulan Terakhir"
       extra={<Button onClick={downloadImage}>Unduh Gambar</Button>}
     >
-      {data && (
+      {/* {data && (
         <Bar
           loading={isLoading}
           onReady={(plot) => {
@@ -48,7 +47,7 @@ function PlotTrendQuestion() {
           }}
           {...config}
         />
-      )}
+      )} */}
     </Card>
   );
 }
