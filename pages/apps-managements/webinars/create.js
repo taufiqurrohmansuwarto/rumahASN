86b9dc3cf3e824ef4b4b1dc94e5cad71a127e5ab
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-06-11 02:57:04 +0700

    downgrade react

--- pages/apps-managements/webinars/create.js
+++ pages/apps-managements/webinars/create.js
@@ -1,6 +1,8 @@
+import PageContainer from "@/components/PageContainer";
 import { adminCreateZoomMeeting } from "@/services/index";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { Button, Form, Input, InputNumber } from "antd";
+import { Button, Card, Col, Form, Input, InputNumber, Row } from "antd";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 const { default: Layout } = require("@/components/Layout");
@@ -24,36 +26,52 @@ const AdminCreateWebinar = () => {
   };
 
   return (
-    <div>
-      <Form
-        form={form}
-        initialValues={{
-          duration: 60,
-        }}
-        onFinish={createWebinar}
-        layout="vertical"
-      >
-        {/* create meeting from zoom api */}
-        <Form.Item name="topic" label="Topic">
-          <Input />
-        </Form.Item>
-        <Form.Item name="agenda" label="Agenda">
-          <Input.TextArea placeholder="Agenda" />
-        </Form.Item>
-        <Form.Item name="duration" help="Durasi Dalam Jam" label="Durasi">
-          <InputNumber placeholder="Duration" />
-        </Form.Item>
-        <Form.Item>
-          <Button
-            disabled={isLoadingCreateMeeting}
-            loading={isLoadingCreateMeeting}
-            htmlType="submit"
-          >
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+    <>
+      <Head>
+        <title>Rumah ASN - Buat Webinar</title>
+      </Head>
+      <PageContainer title="Webinar" subTitle="Buat Webinar">
+        <Row>
+          <Col md={18}>
+            <Card>
+              <Form
+                form={form}
+                initialValues={{
+                  duration: 60,
+                }}
+                onFinish={createWebinar}
+                layout="vertical"
+              >
+                {/* create meeting from zoom api */}
+                <Form.Item name="topic" label="Topic">
+                  <Input />
+                </Form.Item>
+                <Form.Item name="agenda" label="Agenda">
+                  <Input.TextArea placeholder="Agenda" />
+                </Form.Item>
+                <Form.Item
+                  name="duration"
+                  help="Durasi Dalam Jam"
+                  label="Durasi"
+                >
+                  <InputNumber placeholder="Duration" />
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    disabled={isLoadingCreateMeeting}
+                    loading={isLoadingCreateMeeting}
+                    htmlType="submit"
+                  >
+                    Buat
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </PageContainer>
+    </>
   );
 };
 
