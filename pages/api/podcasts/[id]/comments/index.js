88cf77d49581bf052090f65a2fefe1e83a8a2425
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-06-19 16:20:15 +0700

    add controller

--- pages/api/podcasts/[id]/comments/index.js
+++ pages/api/podcasts/[id]/comments/index.js
@@ -1,8 +1,12 @@
+import {
+  commentsPodcasts,
+  createCommentPodcast,
+} from "@/controller/podcasts-comments.controller";
 import auth from "@/middleware/auth.middleware";
 import { createRouter } from "next-connect";
 
 const router = createRouter();
 
-router.use(auth).get().post();
+router.use(auth).get(commentsPodcasts).post(createCommentPodcast);
 
 export default router.handler();
