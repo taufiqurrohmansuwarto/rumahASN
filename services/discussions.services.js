3de76870c422472cb6bc0a7b748fb07842b20963
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-08-18 20:37:55 +0700

    add posts

--- services/discussions.services.js
+++ services/discussions.services.js
@@ -4,12 +4,33 @@ const api = axios.create({
   baseURL: "/helpdesk/api/discussions",
 });
 
+// threads
 export const threads = () => api.get("/threads").then((res) => res.data);
+
 export const thread = (id) => api.get(`/threads/${id}`).then((res) => res.data);
+
 export const createThread = (data) =>
   api.post("/threads", data).then((res) => res.data);
+
 export const updateThread = ({ id, data }) =>
   api.patch(`/threads/${id}`, data).then((res) => res.data);
 
 export const removeThread = (id) =>
   api.delete(`/threads/${id}`).then((res) => res.data);
+
+// posts
+export const posts = (threadId) =>
+  api.get(`/posts?threadId=${threadId}`).then((res) => res.data);
+
+export const post = (id) => api.get(`/posts/${id}`).then((res) => res.data);
+
+export const createPost = ({ threadId, data }) =>
+  api.post(`/posts?threadId=${threadId}`, data).then((res) => res.data);
+
+export const updatePost = ({ id, data }) =>
+  api.patch(`/posts/${id}`, data).then((res) => res.data);
+
+export const removePost = (id) =>
+  api.delete(`/posts/${id}`).then((res) => res.data);
+
+// comments
