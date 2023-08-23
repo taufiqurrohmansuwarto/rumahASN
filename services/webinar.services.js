fc95e34f7b5fe1305cd10a38ba4e92ff79adcaf1
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-08-23 20:23:06 +0700

    persiapan webinar series

---
+++ services/webinar.services.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import queryString from "query-string";
+
+const api = axios.create({
+  baseURL: "/helpdesk/api/webinar-series",
+});
+
+// admin
+export const createWebinar = (data) => {
+  return api.post(`/admin`, data).then((res) => res?.data);
+};
+
+export const readAllWebinar = (query) => {
+  const queryStr = queryString.stringify(query);
+  return api.get(`/admin?${queryStr}`).then((res) => res?.data);
+};
+
+export const updateWebinar = ({ id, data }) => {
+  return api.patch(`/admin/${id}`, data).then((res) => res?.data);
+};
+
+export const removeWebinar = (id) => {
+  return api.delete(`/admin/${id}`).then((res) => res?.data);
+};
+
+// user
+export const readAllWebinarUser = (query) => {
+  const queryStr = queryString.stringify(query);
+  return api.get(`?${queryStr}`).then((res) => res?.data);
+};
+
+export const detailWebinarUser = (id) => {
+  return api.get(`/${id}`).then((res) => res?.data);
+};
+
+export const registerWebinar = (id) => {
+  return api.patch(`/${id}`).then((res) => res?.data);
+};
+
+export const unregisterWebinar = (id) => {
+  return api.delete(`/${id}`).then((res) => res?.data);
+};
