3bdea5a2016ef5e45f7f362370f9c227ebb6488c
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-08-25 16:59:38 +0700

    add something

--- utils/webinar.utils.js
+++ utils/webinar.utils.js
@@ -1,8 +1,16 @@
+import { gotenberg, pipe } from "gotenberg-js-client";
 import axios from "axios";
 
+const toPdf = await pipe(
+  gotenberg("http://localhost:3000"),
+  gotenberg.convert.office("document.docx"),
+  gotenberg.store()
+);
+
 module.exports.wordToPdf = async (req, url) => {
-  const result = await axios.get(url, {
-    responseType: "arraybuffer",
-  });
+  const response = await axios.get(url, { responseType: "arraybuffer" });
+  const wordBuffer = Buffer.from(response.data, "binary");
+
+  const client = gotenberg("http://localhost:3000");
 };
 module.exports.signCertificate = async () => {};
