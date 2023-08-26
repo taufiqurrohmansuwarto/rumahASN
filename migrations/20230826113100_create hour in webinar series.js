706a8c309aa945bfb8d5abaac46f330366b4d33f
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-08-26 18:33:18 +0700

    add certificate number

---
+++ migrations/20230826113100_create hour in webinar series.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("webinar_series", (table) => {
+    table.integer("hour").defaultTo(0);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("webinar_series");
+};
---
+++ migrations/20230826113232_alter_webinar_series_participates_add_certificate_number.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("webinar_series_participates", (table) => {
+    table.string("certificate_number");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("webinar_series_participates");
+};
