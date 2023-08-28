213d58ed9edc79dd11f7214131c9642dd577193a
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-08-28 11:24:40 +0700

    last commit

--- next.config.js
+++ next.config.js
@@ -94,6 +94,12 @@ module.exports = withAntdLess({
         });
       });
 
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
+
     return config;
   },
 });
