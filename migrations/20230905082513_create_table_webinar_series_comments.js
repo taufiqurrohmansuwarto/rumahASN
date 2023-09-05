b9d6353ec6f880621e852c4a57ce0dd415c0a813
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-09-05 15:31:13 +0700

    last commit

--- migrations/20230905082513_create_table_webinar_series_comments.js
+++ migrations/20230905082513_create_table_webinar_series_comments.js
@@ -2,14 +2,34 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("webinar_series_comments", function (table) {
+    table.string("id").primary();
+    table.string("webinar_series_id");
+    table.string("user_id");
+    table.string("comment");
+    table.boolean("is_edited").defaultTo(false);
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+    table.timestamp("deleted_at");
+
+    table
+      .foreign("webinar_series_id")
+      .references("webinar_series.id")
+      .onDelete("cascade")
+      .onUpdate("cascade");
+    table
+      .foreign("user_id")
+      .references("users.custom_id")
+      .onDelete("cascade")
+      .onUpdate("cascade");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("webinar_series_comments");
 };
