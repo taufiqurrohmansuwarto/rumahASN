7c12e17ca6e9f78d1c10e48e4da1bc3acbd645c8
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-09-08 21:02:03 +0700

    last commit

--- pages/api/webinar-series/users/[id]/comments/index.js
+++ pages/api/webinar-series/users/[id]/comments/index.js
@@ -3,9 +3,14 @@ import {
   commentUserIndex,
 } from "@/controller/webinar-series-comments.controller";
 import auth from "@/middleware/auth.middleware";
+import webinarUserTypeMiddleware from "@/middleware/webinar-user-type.middleware";
 import { createRouter } from "next-connect";
 const router = createRouter();
 
-router.use(auth).get(commentUserIndex).post(commentUserCreate);
+router
+  .use(auth)
+  .use(webinarUserTypeMiddleware)
+  .get(commentUserIndex)
+  .post(commentUserCreate);
 
 export default router.handler({});
--- pages/api/webinar-series/users/[id]/index.js
+++ pages/api/webinar-series/users/[id]/index.js
@@ -1,8 +1,9 @@
 import { detailWebinarUser } from "@/controller/webinar-series.controller";
 import auth from "@/middleware/auth.middleware";
+import webinarUserTypeMiddleware from "@/middleware/webinar-user-type.middleware";
 import { createRouter } from "next-connect";
 const router = createRouter();
 
-router.use(auth).get(detailWebinarUser);
+router.use(auth).use(webinarUserTypeMiddleware).get(detailWebinarUser);
 
 export default router.handler({});
