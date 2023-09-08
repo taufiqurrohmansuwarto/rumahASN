f17aa81b2eea1bb5d6c4a6c4c753e7ef7cf9e99b
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-09-09 05:26:15 +0700

    add router to check certificate

---
+++ controller/certificate.controller.js
@@ -0,0 +1,26 @@
+const WebinarSeries = require("@/models/webinar-series.model");
+const WebinarSeriesParticipates = require("@/models/webinar-series-participates.model");
+
+const checkWebinarCertificates = async (req, res) => {
+  try {
+    const { id } = req?.query;
+    const result = await WebinarSeriesParticipates.query().findById(id);
+
+    if (!result) {
+      res.json(null);
+    } else {
+      const data = await WebinarSeriesParticipates.query()
+        .findById(id)
+        .withGraphFetched("[webinar_series, participant]");
+
+      res.json(data);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = {
+  checkWebinarCertificates,
+};
