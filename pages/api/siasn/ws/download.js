80da7de6a963517ae7da70f2d280efaf2fd5b472
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-09-29 11:36:31 +0700

    last commit

--- pages/api/siasn/ws/download.js
+++ pages/api/siasn/ws/download.js
@@ -1,10 +1,15 @@
 import { downloadDocument } from "@/controller/siasn.controller";
+import adminFasilitatorAsnMiddleware from "@/middleware/admin-fasilitator-asn.middleware";
 import asnMiddleware from "@/middleware/asn.middleware";
 import auth from "@/middleware/auth.middleware";
 import { siasnMiddleware } from "@/middleware/siasn.middleware";
 import { createRouter } from "next-connect";
 const router = createRouter();
 
-router.use(auth).use(asnMiddleware).use(siasnMiddleware).get(downloadDocument);
+router
+  .use(auth)
+  .use(adminFasilitatorAsnMiddleware)
+  .use(siasnMiddleware)
+  .get(downloadDocument);
 
 export default router.handler();
