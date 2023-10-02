b6ecc64ce3e033e9a8d97b06ff3f54b1970611d1
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-10-02 12:16:20 +0700

    last commit

--- src/components/PemutakhiranData/AnomaliUser.js
+++ src/components/PemutakhiranData/AnomaliUser.js
@@ -1,26 +1,54 @@
-import { getUserAnomali2023 } from "@/services/anomali.services";
+import {
+  getUserAnomali2023,
+  patchAnomaliUser2023,
+} from "@/services/anomali.services";
 import {
   ExclamationCircleOutlined,
   QuestionCircleOutlined,
 } from "@ant-design/icons";
-import { useQuery } from "@tanstack/react-query";
-import { Button, Skeleton, Tag, Tooltip } from "antd";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button, Modal, Skeleton, Tag, Tooltip, message } from "antd";
 import { useRouter } from "next/router";
 
 const textAnomali = (jenis) => {
   if (jenis === "FORMASI_JF_BELUMDIANGKAT") {
-    return "Anda terkena data anomali 2023 Formasi JF Belum Diangkat. Segera input data JF Anda di menu Jabatan";
+    return "Anda terkena data anomali 2023 Formasi JF Belum Diangkat. Segera input data JF tebaru Anda di menu Jabatan";
   } else if (jenis === "UNOR_NONAKTIF") {
-    return "Anda terkena data anomali 2023 Unor Non Aktif. Segera perbarui Jabatan Anda di menu Jabatan";
+    return "Anda terkena data anomali 2023 Unor Non Aktif. Segera perbarui Jabatan anda menggunakan awalan UPT ...  bagian unor di Jabatan";
   }
 };
 
 function AnomaliUser() {
   const router = useRouter();
+  const queryClient = useQueryClient();
 
   const { data, isLoading } = useQuery(["anomali-user"], () =>
     getUserAnomali2023()
   );
+
+  const { mutateAsync: update, isLoading: isLoadingUpdate } = useMutation(
+    (data) => patchAnomaliUser2023(data),
+    {
+      onSuccess: () => {
+        message.success("Berhasil memperbarui data");
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries(["anomali-user"]);
+      },
+    }
+  );
+
+  const handleUpdate = async (id) => {
+    Modal.confirm({
+      title: "Konfirmasi",
+      content:
+        "Apakah Anda yakin sudah memperbaiki data ini?. Pastikan kembali data yang Anda input sudah benar",
+      centered: true,
+      onOk: async () => {
+        await update({ id, data: { is_repaired: true } });
+      },
+    });
+  };
 
   const gotoPertanyaan = () => {
     router.push("/tickets/create");
@@ -37,11 +65,10 @@ function AnomaliUser() {
                 key={item?.id}
               >
                 <Tag
-                  style={{
-                    cursor: "pointer",
-                  }}
+                  style={{ cursor: "pointer" }}
                   color={item?.is_repaired ? "green" : "red"}
                   icon={<ExclamationCircleOutlined />}
+                  onClick={async () => await handleUpdate(item?.id)}
                 >
                   {item?.jenis_anomali_nama}
                 </Tag>
