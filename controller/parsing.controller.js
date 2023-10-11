934346a7bd166ec484a084230cec7b4259b10024
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-10-11 07:57:08 +0700

    fix markdown

--- controller/parsing.controller.js
+++ controller/parsing.controller.js
@@ -1,10 +1,11 @@
 const { parseMarkdown } = require("@/utils/parsing");
+const { marked } = require("marked");
 
 const markdownToHTML = async (req, res) => {
   try {
     const body = req.body;
-    const html = parseMarkdown(body.text);
-    console.log(html);
+    // const html = parseMarkdown(body.text);
+    const html = marked(body.text);
     res.status(200).json({ html });
   } catch (error) {
     console.log(error);
