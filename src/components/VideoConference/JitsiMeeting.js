96f219c7e4d48e55aa4334826438864fb20bdc1b
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-10-22 23:42:08 +0700

    add jitsi components

---
+++ src/components/VideoConference/JitsiMeeting.js
@@ -0,0 +1,8 @@
+import dynamic from "next/dynamic";
+
+const JitsiMeeting = dynamic(
+  () => import("@jitsi/react-sdk").then((mod) => mod.JitsiMeeting),
+  { ssr: false }
+);
+
+export default JitsiMeeting;
