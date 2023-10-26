9a302b72384ceb2b57505dbc58cd28061a51166f
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-10-26 07:33:16 +0700

    add coaching clinic consultant

---
+++ pages/coaching-clinic-consultant/[id]/detail.js
@@ -0,0 +1,33 @@
+import CreateCoaching from "@/components/CoachingClinic/Consultant/CreateCoaching";
+import Layout from "@/components/Layout";
+import PageContainer from "@/components/PageContainer";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+const DetailCoachingClinic = () => {
+  const router = useRouter();
+
+  const handleCreate = () => router.push("/coaching-clinic/consults");
+
+  return (
+    <>
+      <Head>
+        <title>Rumah ASN - Instruktur Coaching Clinic</title>
+      </Head>
+      <PageContainer title="Coaching Clinic" content="Jadwal Konsultasi Online">
+        <CreateCoaching />
+      </PageContainer>
+    </>
+  );
+};
+
+DetailCoachingClinic.Auth = {
+  action: "manage",
+  subject: "tickets",
+};
+
+DetailCoachingClinic.getLayout = (page) => {
+  return <Layout active="/coaching-clinic-consultant">{page}</Layout>;
+};
+
+export default DetailCoachingClinic;
