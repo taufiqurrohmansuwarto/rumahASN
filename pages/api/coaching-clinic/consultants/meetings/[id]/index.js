2103e6b697f84e75d7c615b1a881f269804d6fbe
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-10-27 09:59:55 +0700

    add more services

--- pages/api/coaching-clinic/consultants/meetings/[id]/index.js
+++ pages/api/coaching-clinic/consultants/meetings/[id]/index.js
@@ -1,8 +1,12 @@
-import { checkStatusCoaching } from "@/controller/coaching-clinic.controller";
+import {
+  removeMeeting,
+  updateMeeting,
+} from "@/controller/coaching-clinic.controller";
+import { detailMeeting } from "@/controller/zoom.controller";
 import auth from "@/middleware/auth.middleware";
 import { createRouter } from "next-connect";
 const router = createRouter();
 
-router.use(auth).get(checkStatusCoaching);
+router.use(auth).get(detailMeeting).patch(updateMeeting).delete(removeMeeting);
 
 export default router.handler();
