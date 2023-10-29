e60cf2fea26e46b9c7589b9d34d76e965d55792f
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-10-29 19:40:00 +0700

    last commit

--- pages/coaching-clinic-consultant/[id]/detail.js
+++ pages/coaching-clinic-consultant/[id]/detail.js
@@ -1,15 +1,39 @@
 import DetailCoachingMeeting from "@/components/CoachingClinic/Consultant/DetailCoachingMeeting";
 import Layout from "@/components/Layout";
 import PageContainer from "@/components/PageContainer";
+import { Breadcrumb } from "antd";
 import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const DetailCoachingClinic = () => {
+  const router = useRouter();
+
   return (
     <>
       <Head>
         <title>Rumah ASN - Instruktur Coaching Clinic</title>
       </Head>
-      <PageContainer title="Coaching Clinic" content="Jadwal Konsultasi Online">
+      <PageContainer
+        onBack={() => router?.back()}
+        breadcrumbRender={() => (
+          <Breadcrumb>
+            <Breadcrumb.Item>
+              <Link href="/feeds">
+                <a>Beranda</a>
+              </Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <Link href="/coaching-clinic-consultant">
+                <a>Coaching Clinic</a>
+              </Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>Detail Coaching Clinic</Breadcrumb.Item>
+          </Breadcrumb>
+        )}
+        title="Coaching Clinic"
+        content="Detail Konsultasi Coaching Clinic"
+      >
         <DetailCoachingMeeting />
       </PageContainer>
     </>
--- src/components/CoachingClinic/Participant/MyMeetings.js
+++ src/components/CoachingClinic/Participant/MyMeetings.js
@@ -1,42 +1,143 @@
-import { meetingsParticipant } from "@/services/coaching-clinics.services";
-import { useQuery } from "@tanstack/react-query";
-import { Table } from "antd";
+import {
+  cancelRequestMeeting,
+  meetingsParticipant,
+} from "@/services/coaching-clinics.services";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Divider, Popconfirm, Space, Table, message } from "antd";
 import { useRouter } from "next/router";
 import React from "react";
+import moment from "moment";
+import { Stack } from "@mantine/core";
 
 function MyMeetings() {
   const router = useRouter();
 
+  const query = router?.query;
+
   const { data, isLoading } = useQuery(
-    ["myMeetings"],
-    () => meetingsParticipant(),
-    {}
+    ["myMeetings", query],
+    () => meetingsParticipant(query),
+    {
+      enabled: !!query,
+      keepPreviousData: true,
+    }
   );
+
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: batal, isLoading: isLoadingBatal } = useMutation(
+    (data) => cancelRequestMeeting(data),
+    {
+      onSuccess: () => {
+        message.success("Berhasil membatalkan coaching clinic");
+        queryClient.invalidateQueries(["myMeetings"]);
+      },
+      onError: (error) => {
+        message.error(error?.response?.data?.message);
+      },
+    }
+  );
+
+  const handleBatal = async (row) => {
+    await batal(row?.meeting?.id);
+  };
 
   const gotoDetailMeetingParticipant = (row) =>
     router.push(`/coaching-clinic/${row?.id}/detail`);
 
   const columns = [
     {
-      title: "title",
+      title: "Coaching Clinic",
+      key: "coaching_clinic",
+      responsive: ["xs"],
+      render: (_, row) => (
+        <>
+          <Stack>
+            <div>{row?.meeting?.title}</div>
+            <div>{row?.meeting?.coach?.username}</div>
+            <div>{row?.meeting?.status}</div>
+            <div>
+              {moment(row?.meeting?.start_date).format("DD MMMM YYYY")}{" "}
+            </div>
+            <div>
+              {row?.meeting?.start_hours} - {row?.meeting?.end_hours}
+            </div>
+            <Space>
+              <a onClick={() => gotoDetailMeetingParticipant(row)}>Detail</a>
+              <Divider type="vertical" />
+              <Popconfirm
+                onConfirm={async () => await handleBatal(row)}
+                title="Apakah anda yakin ingin membatalkan coaching clinic ini?"
+              >
+                <a>Batal</a>
+              </Popconfirm>
+            </Space>
+          </Stack>
+        </>
+      ),
+    },
+    {
+      title: "Judul",
       key: "title",
       render: (_, row) => <>{row?.meeting?.title}</>,
+      responsive: ["sm"],
+    },
+    {
+      title: "Coaching",
+      key: "coaching",
+      render: (_, row) => <>{row?.meeting?.coach?.username}</>,
+      responsive: ["sm"],
+    },
+    {
+      title: "Tanggal",
+      key: "tanggal",
+      render: (_, row) => (
+        <>{moment(row?.meeting?.start_date).format("DD MMMM YYYY")}</>
+      ),
+      responsive: ["sm"],
+    },
+    {
+      title: "Jam",
+      key: "jam",
+      render: (_, row) => (
+        <>
+          {row?.meeting?.start_hours} - {row?.meeting?.end_hours}
+        </>
+      ),
+      responsive: ["sm"],
+    },
+    {
+      title: "Status",
+      key: "status",
+      render: (_, row) => <>{row?.meeting?.status}</>,
+      responsive: ["sm"],
     },
     {
       title: "Aksi",
+      responsive: ["sm"],
       render: (_, row) => {
-        return <a onClick={() => gotoDetailMeetingParticipant(row)}>Detail</a>;
+        return (
+          <>
+            <Space>
+              <a onClick={() => gotoDetailMeetingParticipant(row)}>Detail</a>
+              <Divider type="vertical" />
+              <Popconfirm
+                onConfirm={async () => await handleBatal(row)}
+                title="Apakah anda yakin ingin membatalkan coaching clinic ini?"
+              >
+                <a>Batal</a>
+              </Popconfirm>
+            </Space>
+          </>
+        );
       },
     },
   ];
 
   return (
-    <Table
-      title={() => "hello world"}
-      columns={columns}
-      dataSource={data}
-      loading={isLoading}
-    />
+    <>
+      <Table columns={columns} dataSource={data?.data} loading={isLoading} />
+    </>
   );
 }
 
