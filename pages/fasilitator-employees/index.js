883ab65521f85d74c9651dfa9877afd852288f4e
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-11-10 20:03:06 +0700

    terakhir

--- pages/fasilitator-employees/index.js
+++ pages/fasilitator-employees/index.js
@@ -2,7 +2,7 @@ import Layout from "@/components/Layout";
 import PageContainer from "@/components/PageContainer";
 import { downloadDataAnomaliFasilitator } from "@/services/anomali.services";
 import { useMutation } from "@tanstack/react-query";
-import { Button, message, Form, Input } from "antd";
+import { Button, message, Form, Input, Card } from "antd";
 import { useRouter } from "next/router";
 
 const FasilitatorEmployees = () => {
@@ -44,37 +44,40 @@ const FasilitatorEmployees = () => {
   };
 
   return (
-    <PageContainer>
-      <Button
-        disabled={isLoadingDownload}
-        loading={isLoadingDownload}
-        onClick={handleDownload}
-      >
-        Unduh Data Anomali
-      </Button>
-      <Form
-        layout="vertical"
-        form={form}
-        name="form cari siasn"
-        onFinish={handleFinish}
-      >
-        <Form.Item
-          normalize={(values) => values.replace(/\s/g, "")}
-          rules={[
-            { min: 18, message: "NIP harus 18 karakter" },
-            { required: true, message: "Harus diisi" },
-          ]}
-          name="nip"
-          label="Nomer Induk Pegawai"
+    <PageContainer title="Fasilitator SIMASTER" content="Integrasi SIASN">
+      <Card>
+        <Button
+          disabled={isLoadingDownload}
+          loading={isLoadingDownload}
+          onClick={handleDownload}
         >
-          <Input />
-        </Form.Item>
-        <Form.Item>
-          <Button htmlType="submit" type="primary">
-            Cari
-          </Button>
-        </Form.Item>
-      </Form>
+          Unduh Data Anomali
+        </Button>
+        <Form
+          layout="vertical"
+          form={form}
+          name="form cari siasn"
+          onFinish={handleFinish}
+        >
+          <Form.Item
+            normalize={(values) => values.replace(/\s/g, "")}
+            rules={[
+              { min: 18, message: "NIP harus 18 karakter" },
+              { required: true, message: "Harus diisi" },
+            ]}
+            name="nip"
+            help="Masukkan NIP pegawai sesuai dengan perangkat daerah"
+            label="Nomer Induk Pegawai"
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item>
+            <Button htmlType="submit" type="primary">
+              Cari
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </PageContainer>
   );
 };
