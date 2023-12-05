ed4c990a33a68829a358bb7a6a0ecb1af0a0ff95
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-12-05 09:11:07 +0700

    add kedudukan hukum untuk pensiun

--- src/components/PemutakhiranData/Admin/CompareKedudukanHukumByNip.js
+++ src/components/PemutakhiranData/Admin/CompareKedudukanHukumByNip.js
@@ -1,5 +1,67 @@
+import { rwKedudukanHukumByNip } from "@/services/master.services";
+import { useQuery } from "@tanstack/react-query";
+import { Table } from "antd";
+
 function CompareKedudukanHukumByNip({ nip }) {
-  return <div>{nip}</div>;
+  const { data, isLoading } = useQuery(
+    ["kedudukan-hukum", nip],
+    () => rwKedudukanHukumByNip(nip),
+    { enabled: !!nip }
+  );
+
+  const columns = [
+    {
+      title: "File",
+      dataIndex: "file_sk",
+      render: (_, record) => {
+        return (
+          <div>
+            <a
+              href={record?.file_kedudukan_hukum}
+              target="_blank"
+              rel="noreferrer"
+            >
+              File
+            </a>
+          </div>
+        );
+      },
+    },
+    {
+      title: "Kedudukan Hukum",
+      key: "kedudukan_hukum",
+      render: (_, row) => <>{row?.kedudukan_hukum?.kedudukan_hukum}</>,
+    },
+    {
+      title: "Tgl. SK",
+      dataIndex: "tgl_sk",
+    },
+    {
+      title: "No. SK",
+      dataIndex: "no_sk",
+    },
+    {
+      title: "Keterangan Instansi",
+      dataIndex: "keterangan_instansi",
+    },
+    {
+      title: "Aktif",
+      dataIndex: "aktif",
+    },
+  ];
+
+  return (
+    <>
+      <Table
+        title={() => <h3>Kedudukan Hukum SIMASTER</h3>}
+        columns={columns}
+        rowKey={(row) => row?.kedudukan_hukum_id}
+        pagination={false}
+        dataSource={data}
+        loading={isLoading}
+      />
+    </>
+  );
 }
 
 export default CompareKedudukanHukumByNip;
