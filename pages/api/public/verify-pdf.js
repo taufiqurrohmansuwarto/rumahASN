6627cc8c4a92710f1c9b087872851d30545eb7b5
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-01-23 08:47:24 +0700

    last commit

--- pages/api/public/verify-pdf.js
+++ pages/api/public/verify-pdf.js
@@ -1,6 +1,15 @@
 import { verifyPdfController } from "@/controller/esign.controller";
 import { createRouter } from "next-connect";
 
+// maximum request body size 50mb
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "10mb",
+    },
+  },
+};
+
 const router = createRouter();
 router.post(verifyPdfController);
 export default router.handler({});
