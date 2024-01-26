6056bc81d59e0eaf556ec2da9b15b51e8e62df8a
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-01-26 14:15:54 +0700

    last commit

---
+++ controller/webinar-certificates.controller.js
@@ -0,0 +1,84 @@
+const WebinarParticipates = require("@/models/webinar-series-participates.model");
+const WebinarSeries = require("@/models/webinar-series.model");
+
+const daftarCertificateSigner = async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
+    const employee_number = req.user.employee_number;
+
+    const webinarParticipates = await WebinarSeries.query()
+      .where({
+        employee_number_signer: employee_number,
+        use_personal_signer: true,
+      })
+      .page(page, limit);
+
+    const data = {
+      data: webinarParticipates.results,
+      pagination: {
+        total: webinarParticipates.total,
+        page: webinarParticipates.page,
+        limit: webinarParticipates.limit,
+      },
+    };
+
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const dataCertificateSignerByWebinarId = async (req, res) => {
+  try {
+    const webinar_id = req.query?.id;
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
+
+    const webinarParticipates = await WebinarParticipates.query()
+      .where({ webinar_id })
+      .page(page, limit);
+
+    const data = {
+      data: webinarParticipates.results,
+      pagination: {
+        total: webinarParticipates.total,
+        page: webinarParticipates.page,
+        limit: webinarParticipates.limit,
+      },
+    };
+
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const signCertificateByWebinarId = async (req, res) => {
+  try {
+    const webinar_id = req?.query?.id;
+    const data = req?.body;
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const signCertificateById = async (req, res) => {
+  try {
+    const webinar_participates_id = req?.query?.id;
+    const data = {};
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = {
+  daftarCertificateSigner,
+  dataCertificateSignerByWebinarId,
+  signCertificateById,
+  signCertificateByWebinarId,
+};
