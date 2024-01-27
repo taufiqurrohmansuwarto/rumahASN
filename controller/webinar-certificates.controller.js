787a169b19c1a9821e71a5da2051bcbf66554533
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-01-27 15:59:35 +0700

    add more edit

--- controller/webinar-certificates.controller.js
+++ controller/webinar-certificates.controller.js
@@ -36,20 +36,41 @@ const dataCertificateSignerByWebinarId = async (req, res) => {
     const limit = parseInt(req.query.limit) || 10;
     const page = parseInt(req.query.page) || 1;
 
-    const webinarParticipates = await WebinarParticipates.query()
-      .where({ webinar_id })
-      .page(page, limit);
+    // cek dulu webinar yang sudah berstatus published dan is_allow_download_certificate true
+    const webinar = await WebinarSeries.query()
+      .where({
+        id: webinar_id,
+        is_allow_download_certificate: true,
+        status: "published",
+      })
+      .first();
 
-    const data = {
-      data: webinarParticipates.results,
-      pagination: {
-        total: webinarParticipates.total,
-        page: webinarParticipates.page,
-        limit: webinarParticipates.limit,
-      },
-    };
+    if (!webinar) {
+      const data = {
+        data: [],
+        pagination: {
+          total: 0,
+          page: 1,
+          limit: 10,
+        },
+      };
+      res.json(data);
+    } else {
+      const webinarParticipates = await WebinarParticipates.query()
+        .where({ webinar_id })
+        .page(page, limit);
 
-    res.json(data);
+      const data = {
+        data: webinarParticipates.results,
+        pagination: {
+          total: webinarParticipates.total,
+          page: webinarParticipates.page,
+          limit: webinarParticipates.limit,
+        },
+      };
+
+      res.json(data);
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
