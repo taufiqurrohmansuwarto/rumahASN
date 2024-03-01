32bc6cdc16bcff4a4a8e403170c93f3d75f94b5b
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-03-01 17:34:26 +0700

    last commit

--- controller/webinar-series-posttests.controller.js
+++ controller/webinar-series-posttests.controller.js
@@ -1,10 +1,76 @@
 const WebinarSeriesPosttests = require("@/models/webinar-series-postests.model");
 
-const index = async (req, res) => {};
-const show = async (req, res) => {};
-const store = async (req, res) => {};
-const update = async (req, res) => {};
-const destroy = async (req, res) => {};
+const index = async (req, res) => {
+  try {
+    const { webinarId } = req?.query;
+    const result = await WebinarSeriesPosttests.query().where(
+      "webinar_series_id",
+      webinarId
+    );
+
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const show = async (req, res) => {
+  try {
+    const { posttestId } = req?.query;
+    const result = await WebinarSeriesPosttests.query().findById(posttestId);
+
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const store = async (req, res) => {
+  try {
+    const { body } = req;
+
+    const result = await WebinarSeriesPosttests.query().insert(body);
+
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const { webinarId, posttestId } = req?.query;
+    const { body } = req;
+
+    const result = await WebinarSeriesPosttests.query()
+      .where({
+        id: posttestId,
+        webinar_series_id: webinarId,
+      })
+      .patch(body);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const destroy = async (req, res) => {
+  try {
+    const { webinarId, posttestId } = req?.query;
+    const result = await WebinarSeriesPosttests.query().delete().where({
+      id: posttestId,
+      webinar_series_id: webinarId,
+    });
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 module.exports = {
   index,
