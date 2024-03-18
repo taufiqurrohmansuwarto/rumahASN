e3ad7892db1cbcf0f1ddfd41136d9d6ee30f8c92
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-03-19 05:34:15 +0700

    add form unor jabatan

--- src/components/PemutakhiranData/FormJenisMutasi.js
+++ src/components/PemutakhiranData/FormJenisMutasi.js
@@ -1,7 +1,29 @@
-import React from "react";
+import { refJenisMutasi, refJenisPenugasan } from "@/services/siasn-services";
+import { useQuery } from "@tanstack/react-query";
+import { Form, Select } from "antd";
 
-function FormJenisMutasi() {
-  return <div>FormJenisMutasi</div>;
+function FormJenisMutasi({ name }) {
+  const { data, isLoading } = useQuery(
+    ["ref-jenis-mutasi"],
+    () => refJenisMutasi(),
+    {}
+  );
+
+  return (
+    <>
+      {data && (
+        <Form.Item name={name}>
+          <Select>
+            {data?.map((item) => (
+              <Select.Option key={item?.id} value={item?.id}>
+                {item?.nama}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+      )}
+    </>
+  );
 }
 
 export default FormJenisMutasi;
