04ef720184d4579c1158de4254c0db4bab99ea61
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-03-23 23:52:15 +0700

    last commit

---
+++ migrations/20240323163839_create user json to submissions_pics.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("submissions_pics", function (table) {
+    table.jsonb("user");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table("submissions_pics", function (table) {
+    table.dropColumn("user");
+  });
+};
