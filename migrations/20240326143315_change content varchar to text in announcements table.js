af9206c35980882f26d09294bbe378ff29cd4a44
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-03-26 21:33:51 +0700

    last commit

---
+++ migrations/20240326143315_change content varchar to text in announcements table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("announcements", function (table) {
+    table.text("content").alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("announcements", function (table) {
+    table.string("content", 255).alter();
+  });
+};
