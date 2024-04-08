ed828c3c96b71dc9769c47324687e6a9474ca07a
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-04-09 05:05:00 +0700

    mobile first

--- src/components/Fasilitator/EmployeesTable.js
+++ src/components/Fasilitator/EmployeesTable.js
@@ -1,8 +1,8 @@
+import useScrollRestoration from "@/hooks/useScrollRestoration";
 import { getAllEmployeesPaging } from "@/services/master.services";
 import { capitalizeWords } from "@/utils/client-utils";
 import {
   CheckCircleOutlined,
-  CloudDownloadOutlined,
   ExclamationCircleOutlined,
 } from "@ant-design/icons";
 import { useQuery } from "@tanstack/react-query";
@@ -10,7 +10,6 @@ import {
   Avatar,
   Button,
   Flex,
-  Modal,
   Space,
   Table,
   Tag,
@@ -34,6 +33,7 @@ const TagKomparasi = ({ komparasi, nama }) => {
 
 function EmployeesTable() {
   const router = useRouter();
+  useScrollRestoration();
 
   const { data, isLoading, isFetching } = useQuery(
     ["employees-paging", router?.query],
@@ -57,8 +57,96 @@ function EmployeesTable() {
 
   const columns = [
     {
+      title: "Nama",
+      dataIndex: "nama_master",
+      responsive: ["xs"],
+      render: (_, row) => {
+        return (
+          <div>
+            <Flex gap={8} vertical align="start">
+              <Avatar size={50} shape="square" src={row?.foto} alt="foto" />
+              <Typography.Text>{row?.nama_master}</Typography.Text>
+              <Typography.Text>{row?.nip_master}</Typography.Text>
+              <Tooltip title="Pembetulan dilakukan di SIASN">
+                <Tag color="yellow">{row?.siasn?.status}</Tag>
+              </Tooltip>
+              <Tooltip title="Perbaikan dilakukan dengan menambahkan riwayat jabatan SIASN">
+                <Tag color="cyan">{row?.siasn?.jenjang_jabatan}</Tag>
+              </Tooltip>
+            </Flex>
+            <Flex
+              gap={2}
+              style={{
+                marginTop: 8,
+              }}
+              wrap="wrap"
+            >
+              <Tooltip title="Perbaikan dapat dilakukan dengan usulan perubahan nama">
+                <div>
+                  <TagKomparasi
+                    komparasi={row?.komparasi?.nama}
+                    nama={"Nama"}
+                  />
+                </div>
+              </Tooltip>
+              <Tooltip title="Perbaikan dapat dilakukan dengan usulan perubahan nip">
+                <div>
+                  <TagKomparasi komparasi={row?.komparasi?.nip} nama={"NIP"} />
+                </div>
+              </Tooltip>
+              <Tooltip title="Perbaikan dapat dilakukan dengan usulan perubahan tanggal lahir">
+                <div>
+                  <TagKomparasi
+                    komparasi={row?.komparasi?.tanggal_lahir}
+                    nama={"Tanggal Lahir"}
+                  />
+                </div>
+              </Tooltip>
+
+              <Tooltip title="Perbaikan dapat dilakukan dengan melakukan verifikas NIK di SIASN atau MyASN Personal">
+                <div>
+                  <TagKomparasi komparasi={row?.siasn?.valid_nik} nama="NIK" />
+                </div>
+              </Tooltip>
+              <Tooltip title="Perbaikan dapat dilakukan dengan melakukan perubahan email di SIASN">
+                <div>
+                  <TagKomparasi
+                    komparasi={row?.komparasi?.email}
+                    nama="Email"
+                  />
+                </div>
+              </Tooltip>
+              <TagKomparasi
+                komparasi={row?.komparasi?.pangkat}
+                nama="Pangkat"
+              />
+              <Tooltip title="Apabila tidak sesuai dapat dilakukan usulan pencantuman gelar">
+                <div>
+                  <TagKomparasi
+                    komparasi={row?.komparasi?.pendidikan}
+                    nama="Pendidikan"
+                  />
+                </div>
+              </Tooltip>
+              {row?.status_master !== "PPPK" && (
+                <Tooltip title="Dapat dilakukan pembetulan di peremajaan data SIASN/Rumah ASN">
+                  <div>
+                    <TagKomparasi
+                      komparasi={row?.komparasi?.jenjang_jabatan}
+                      nama="Jenjang Jabatan"
+                    />
+                  </div>
+                </Tooltip>
+              )}
+            </Flex>
+          </div>
+        );
+      },
+    },
+    {
       title: "Foto",
       key: "foto",
+      responsive: ["sm"],
       width: 100,
       render: (row) => (
         <Avatar size={100} shape="square" src={row?.foto} alt="foto" />
@@ -67,6 +155,7 @@ function EmployeesTable() {
     {
       title: "Informasi",
       key: "informasi",
+      responsive: ["sm"],
       width: 200,
       render: (_, row) => {
         return (
@@ -87,6 +176,7 @@ function EmployeesTable() {
     {
       title: "Jabatan",
       key: "jabatan",
+      responsive: ["sm"],
       render: (row) => {
         return (
           <Space direction="vertical" size="small">
@@ -103,6 +193,7 @@ function EmployeesTable() {
     {
       title: "Jenjang/Pendidikan",
       key: "jenjang_pendidikan_master",
+      responsive: ["sm"],
       render: (row) => (
         <Space direction="vertical" size="small">
           {row?.jenjang_master || "SIMASTER Kosong"}
@@ -116,17 +207,20 @@ function EmployeesTable() {
     {
       title: "Perangkat Daerah",
       dataIndex: "opd_master",
+      responsive: ["sm"],
       width: 200,
     },
     {
       title: "Unor SIASN",
       key: "unor_siasn",
+      responsive: ["sm"],
       width: 200,
       render: (row) => <div>{row?.siasn?.unor}</div>,
     },
     {
       title: "Hasil Validasi",
       key: "hasil",
+      responsive: ["sm"],
       width: 100,
       render: (row) => {
         return (
@@ -199,6 +293,7 @@ function EmployeesTable() {
     {
       title: "Aksi",
       key: "action",
+      responsive: ["xs"],
       render: (row) => {
         return <a onClick={() => gotoDetail(row?.nip_master)}>Detail</a>;
       },
