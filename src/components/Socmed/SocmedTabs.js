e8df55bc3a8e19a647db3e9b897190a399f1b8fb
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-05-26 22:50:34 +0700

    add box menu in asn updates

--- src/components/Socmed/SocmedTabs.js
+++ src/components/Socmed/SocmedTabs.js
@@ -1,6 +1,15 @@
 import useScrollRestoration from "@/hooks/useScrollRestoration";
-import { Stack } from "@mantine/core";
-import { Card, Col, Row, Tabs } from "antd";
+import { Carousel } from "@mantine/carousel";
+import { Stack, Text } from "@mantine/core";
+import {
+  IconDatabase,
+  IconDeviceComputerCamera,
+  IconHistory,
+  IconMapPin,
+  IconMessage,
+  IconUsers,
+} from "@tabler/icons";
+import { Affix, Card, Col, Flex, Row, Tabs } from "antd";
 import { useRouter } from "next/router";
 import Announcement from "../Announcement";
 import SocmedCreatePost from "./SocmedCreatePost";
@@ -29,6 +38,144 @@ function SocmedTabs() {
             <Col md={24} xs={24}>
               <Stack spacing="sm">
                 <Announcement />
+                <Affix offsetTop={50}>
+                  <Card>
+                    <Carousel
+                      slideSize="200"
+                      height={42}
+                      slideGap="sm"
+                      align="start"
+                      controlsOffset="md"
+                      controlSize={23}
+                      dragFree
+                    >
+                      <Carousel.Slide>
+                        <Flex
+                          align="center"
+                          justify="center"
+                          style={{
+                            height: "100%",
+                            borderRadius: 18,
+                            border: "1px solid #d9d9d9",
+                            cursor: "pointer",
+                          }}
+                          onClick={() =>
+                            router.push("/pemutakhiran-data/komparasi")
+                          }
+                        >
+                          <Flex align="center" gap={8}>
+                            <IconDatabase color="blue" size={18} />
+                            <Text size="sm" fw={700}>
+                              Data MyASN
+                            </Text>
+                          </Flex>
+                        </Flex>
+                      </Carousel.Slide>
+                      <Carousel.Slide>
+                        <Flex
+                          align="center"
+                          justify="center"
+                          style={{
+                            cursor: "pointer",
+                            height: "100%",
+                            borderRadius: 18,
+                            border: "1px solid #d9d9d9",
+                          }}
+                          onClick={() =>
+                            router.push(
+                              "/pemutakhiran-data/usulan-siasn/kenaikan-pangkat"
+                            )
+                          }
+                        >
+                          <Flex align="center" gap={8}>
+                            <IconHistory color="red" size={18} />
+                            <Text size="sm" fw={700}>
+                              Tracking Usulan
+                            </Text>
+                          </Flex>
+                        </Flex>
+                      </Carousel.Slide>
+                      <Carousel.Slide>
+                        <Flex
+                          align="center"
+                          justify="center"
+                          style={{
+                            height: "100%",
+                            borderRadius: 18,
+                            border: "1px solid #d9d9d9",
+                            cursor: "pointer",
+                          }}
+                          onClick={() => router.push("/webinar-series/all")}
+                        >
+                          <Flex align="center" gap={8}>
+                            <IconDeviceComputerCamera color="blue" size={18} />
+                            <Text size="sm" fw={700}>
+                              Webinar ASN
+                            </Text>
+                          </Flex>
+                        </Flex>
+                      </Carousel.Slide>
+                      <Carousel.Slide>
+                        <Flex
+                          align="center"
+                          justify="center"
+                          style={{
+                            height: "100%",
+                            borderRadius: 18,
+                            border: "1px solid #d9d9d9",
+                            cursor: "pointer",
+                          }}
+                          onClick={() => router.push("/feeds")}
+                        >
+                          <Flex align="center" gap={8}>
+                            <IconMessage color="orange" size={18} />
+                            <Text size="sm" fw={700}>
+                              Forum Kepegawaian
+                            </Text>
+                          </Flex>
+                        </Flex>
+                      </Carousel.Slide>
+                      <Carousel.Slide>
+                        <Flex
+                          align="center"
+                          justify="center"
+                          style={{
+                            height: "100%",
+                            borderRadius: 18,
+                            border: "1px solid #d9d9d9",
+                            cursor: "pointer",
+                          }}
+                          onClick={() => router.push("/coaching-clinic/all")}
+                        >
+                          <Flex align="center" gap={8}>
+                            <IconUsers color="green" size={18} />
+                            <Text size="sm" fw={700}>
+                              Coaching Clinic
+                            </Text>
+                          </Flex>
+                        </Flex>
+                      </Carousel.Slide>
+                      <Carousel.Slide>
+                        <Flex
+                          align="center"
+                          justify="center"
+                          style={{
+                            height: "100%",
+                            borderRadius: 18,
+                            border: "1px solid #d9d9d9",
+                          }}
+                        >
+                          <Flex align="center" gap={8}>
+                            <IconMapPin color="blue" size={18} />
+                            <Text size="sm" fw={700}>
+                              Kegiatan ASN
+                            </Text>
+                          </Flex>
+                        </Flex>
+                      </Carousel.Slide>
+                    </Carousel>
+                  </Card>
+                </Affix>
                 <Card>
                   <SocmedCreatePost />
                 </Card>
