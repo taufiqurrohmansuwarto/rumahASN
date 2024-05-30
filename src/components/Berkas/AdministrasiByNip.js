fe74e8a63c1afa4aad3556ad61f238eb41b1fbf0
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-05-30 14:09:34 +0700

    last commit

--- src/components/Berkas/AdministrasiByNip.js
+++ src/components/Berkas/AdministrasiByNip.js
@@ -8,7 +8,7 @@ import { Button, Space, Typography } from "antd";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
-const list_tmt = ["01042024", "01052024"];
+const list_tmt = ["01042024", "01052024", "01062024"];
 const dokumen = ["SK", "PERTEK", "SPMT", "PK"];
 
 const Tombol = ({ tmt, file, nip }) => {
