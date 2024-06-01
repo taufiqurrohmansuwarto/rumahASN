5bb8b3a394343ddc35a03a6a3ac603e660ec1885
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-06-01 20:48:23 +0700

    add refresh jabatan,golongan, pendidikan dan pernikahan di siasn controller

--- controller/siasn-proxy.controller.js
+++ controller/siasn-proxy.controller.js
@@ -4,6 +4,110 @@ import axios from "axios";
 
 const apiGateway = process.env.APIGATEWAY_URL;
 
+const refreshAttributeByNip = async (fetcher, attribute, nip) => {
+  try {
+    const result = await fetcher.get(
+      `/siasn-ws/proxy/pns/refresh-${attribute}/${nip}`
+    );
+
+    const data = result?.data;
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+// refresh golongan,jabatan, pendidikan, dan pernikahan
+const refreshGolongan = async (req, res) => {
+  try {
+    const { employee_number: nip } = req?.user;
+    const result = await refreshAttributeByNip(req.fetcher, "golongan", nip);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+};
+
+const refreshGolonganByNip = async (req, res) => {
+  try {
+    const { nip } = req.query;
+    const result = await refreshAttributeByNip(req.fetcher, "golongan", nip);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+};
+
+const refreshJabatan = async (req, res) => {
+  try {
+    const { employee_number: nip } = req?.user;
+    const result = await refreshAttributeByNip(req.fetcher, "jabatan", nip);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+};
+
+const refreshJabatanByNip = async (req, res) => {
+  try {
+    const { nip } = req.query;
+    const result = await refreshAttributeByNip(req.fetcher, "jabatan", nip);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+};
+
+const refreshPendidikan = async (req, res) => {
+  try {
+    const { employee_number: nip } = req?.user;
+    const result = await refreshAttributeByNip(req.fetcher, "pendidikan", nip);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+};
+
+const refreshPendidikanByNip = async (req, res) => {
+  try {
+    const { nip } = req.query;
+    const result = await refreshAttributeByNip(req.fetcher, "pendidikan", nip);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+};
+
+const refreshPernikahan = async (req, res) => {
+  try {
+    const { employee_number: nip } = req?.user;
+    const result = await refreshAttributeByNip(req.fetcher, "pernikahan", nip);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+};
+
+const refreshPernikahanByNip = async (req, res) => {
+  try {
+    const { nip } = req.query;
+    const result = await refreshAttributeByNip(req.fetcher, "pernikahan", nip);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+};
+
 const cariPnsKinerjaProxy = async (req, res) => {
   try {
     const { nip } = req.query;
@@ -34,4 +138,12 @@ const cariPnsKinerjaProxy = async (req, res) => {
 
 module.exports = {
   cariPnsKinerjaProxy,
+  refreshGolongan,
+  refreshGolonganByNip,
+  refreshJabatan,
+  refreshJabatanByNip,
+  refreshPendidikan,
+  refreshPendidikanByNip,
+  refreshPernikahan,
+  refreshPernikahanByNip,
 };
