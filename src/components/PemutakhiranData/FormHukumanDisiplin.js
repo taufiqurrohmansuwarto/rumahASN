613a51a7d9086b2bf3be606061107d3ff8d3e782
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-06-08 12:48:00 +0700

    fix jenis jabatan id

---
+++ src/components/PemutakhiranData/FormHukumanDisiplin.js
@@ -0,0 +1,25 @@
+import {
+  dataHukumanDisiplin,
+  dataRiwayatPenghargaan,
+} from "@/utils/client-data";
+import { Form, Input, Select } from "antd";
+
+const FormHukumanDisiplin = ({ name, label }) => {
+  const [form] = Form.useForm();
+
+  return (
+    <Form form={form}>
+      <Form.Item name={name} label={label}>
+        <Select>
+          {dataHukumanDisiplin.map((item) => (
+            <Select.Option key={item.id} value={item.id}>
+              {item.nama}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default FormHukumanDisiplin;
