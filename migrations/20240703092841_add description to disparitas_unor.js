25036cce2b48dc6a5c54df9f5260627b3d927c59
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-07-03 17:00:38 +0700

    last commit

---
+++ migrations/20240703092841_add description to disparitas_unor.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("disparitas_unor", (table) => {
+    table.text("description");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("disparitas_unor", (table) => {
+    table.dropColumn("description");
+  });
+};
