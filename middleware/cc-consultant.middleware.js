0e278b0592c74b6092b9050bac28a8a89b3bf7e0
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-08-12 18:19:35 +0700

    last commit

--- middleware/cc-consultant.middleware.js
+++ middleware/cc-consultant.middleware.js
@@ -1,6 +1,12 @@
 module.exports = (req, res, next) => {
   const { user } = req;
-  if (user?.is_consultant) {
+
+  const group = user?.group;
+  const role = user?.role;
+
+  const fasilitatorMaster = group === "FASILITATOR" && role === "MASTER";
+
+  if (user?.is_consultant || fasilitatorMaster) {
     next();
   } else {
     res.status(403).json({ code: 403, message: "Forbidden" });
