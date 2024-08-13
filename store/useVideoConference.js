44335f54a9c504d5e6f3d72b1ab65ff7948cc5be
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-08-13 12:47:35 +0700

    last commit

--- store/useVideoConference.js
+++ store/useVideoConference.js
@@ -1,22 +1,55 @@
 import { create } from "zustand";
 
+const calculateCenterPosition = (width, height) => {
+  if (typeof window === "undefined") {
+    return { x: 0, y: 0 }; // Default position for server-side rendering
+  }
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const x = Math.max(0, Math.floor((windowWidth - width) / 2));
+  const y = Math.max(0, Math.floor((windowHeight - height) / 2));
+  return { x, y };
+};
+
+const DEFAULT_SIZE = { width: 800, height: 600 };
+const MINIMIZED_SIZE = { width: 300, height: 200 };
+
 // Zustand store
 const useVideoConferenceStore = create((set) => ({
   isOpen: false,
-  position: { x: 20, y: 20 },
-  size: { width: 800, height: 600 },
+  position: calculateCenterPosition(DEFAULT_SIZE.width, DEFAULT_SIZE.height),
+  size: DEFAULT_SIZE,
   isMinimized: false,
   openVideoConference: () => set({ isOpen: true, isMinimized: false }),
   closeVideoConference: () => set({ isOpen: false }),
   updatePosition: (newPosition) => set({ position: newPosition }),
-  updateSize: (newSize) => set({ size: newSize }),
+  updateSize: (newSize) =>
+    set((state) => {
+      const updatedSize = { ...state.size, ...newSize };
+      return {
+        size: updatedSize,
+        position: calculateCenterPosition(
+          updatedSize.width,
+          updatedSize.height
+        ),
+      };
+    }),
   toggleMinimize: () =>
-    set((state) => ({
-      isMinimized: !state.isMinimized,
-      size: state.isMinimized
-        ? { width: 800, height: 600 }
-        : { width: 300, height: 200 },
-    })),
+    set((state) => {
+      const newSize = state.isMinimized ? DEFAULT_SIZE : MINIMIZED_SIZE;
+      return {
+        isMinimized: !state.isMinimized,
+        size: newSize,
+        position: calculateCenterPosition(newSize.width, newSize.height),
+      };
+    }),
 }));
+
+if (typeof window !== "undefined") {
+  window.addEventListener(
+    "resize",
+    useVideoConferenceStore.getState().handleResize
+  );
+}
 
 export default useVideoConferenceStore;
