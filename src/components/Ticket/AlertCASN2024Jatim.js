6f596b4c9d03797b1a4877abd40897e7bca3e9b3
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-09-19 07:53:25 +0700

    last commit

--- src/components/Ticket/AlertCASN2024Jatim.js
+++ src/components/Ticket/AlertCASN2024Jatim.js
@@ -1,26 +1,38 @@
-import { Alert } from "@mantine/core";
+import { Alert, Stack, Image } from "@mantine/core";
 import { IconInfoCircle } from "@tabler/icons";
 
 function AlertCASN2024Jatim() {
   return (
-    <Alert
-      color="yellow"
-      mb="md"
-      title="Pengumuman CASN 2024 Jatim"
-      variant="outline"
-      icon={<IconInfoCircle />}
-    >
-      <p>
-        Untuk informasi lebih lanjut mengenai CASN 2024 Jatim, silakan kunjungi{" "}
-        <a
-          href="https://bkd.jatimprov.go.id/Rekrutmen-ASN2024/bacpeng/pengumuman-rekrutmen-cpns-pemprov-jatim-tahun-2024"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Pengumuman Rekrutmen ASN Pemprov Jatim Tahun 2024
-        </a>
-      </p>
-    </Alert>
+    <Stack>
+      {/* <Alert
+        color="yellow"
+        mb="md"
+        title="Pengumuman CASN 2024 Jatim"
+        variant="outline"
+        icon={<IconInfoCircle />}
+      >
+        <p>
+          Untuk informasi lebih lanjut mengenai CASN 2024 Jatim, silakan
+          kunjungi{" "}
+          <a
+            href="https://bkd.jatimprov.go.id/Rekrutmen-ASN2024/bacpeng/pengumuman-rekrutmen-cpns-pemprov-jatim-tahun-2024"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Pengumuman Rekrutmen ASN Pemprov Jatim Tahun 2024
+          </a>
+        </p>
+      </Alert> */}
+      <a href="https://sscasn.bkn.go.id/" target={"_blank"} rel={"noreferrer"}>
+        <Image
+          mb={10}
+          src={
+            "https://siasn.bkd.jatimprov.go.id:9000/public/banner-sscasn.png"
+          }
+          alt="banner-sscasn.webp"
+        />
+      </a>
+    </Stack>
   );
 }
 
