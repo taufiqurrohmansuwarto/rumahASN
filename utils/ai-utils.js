3d43b3d09c39feed100794955330f6424cc3d9c5
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-10-29 18:01:34 +0700

    add more function to search something

---
+++ utils/ai-utils.js
@@ -0,0 +1,41 @@
+const SyncPegawai = require("@/models/sync-pegawai.model");
+
+module.exports.cariSeluruhRekanKerja = async (currentOpdId) => {
+  try {
+    // 12301 menjadi 123
+    const currentUserDepartmentCode = currentOpdId.substring(0, 3);
+    const employees = await SyncPegawai.query().where(
+      "skpd_id",
+      "ilike",
+      `${currentUserDepartmentCode}%`
+    );
+
+    if (!employees) {
+      throw new Error("Employee not found");
+    } else {
+      return employees;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports.cariRekanKerjaDiBidang = async (currentOpdId) => {
+  try {
+    // 1230101 menjadi 12301 ambil 2 string dibelakang jangan langsung ke 5
+    const currentUserDepartmentCode = currentOpdId?.toString().slice(0, -2);
+    const employees = await SyncPegawai.query().where(
+      "skpd_id",
+      "ilike",
+      `${currentUserDepartmentCode}%`
+    );
+
+    if (!employees) {
+      throw new Error("Employee not found");
+    } else {
+      return employees;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
