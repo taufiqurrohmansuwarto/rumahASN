a6c27918394e77cbce7f46a471999222992612e9
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-11-24 07:46:20 +0700

    add bot ui

--- pages/chat-ai/index.js
+++ pages/chat-ai/index.js
@@ -2,17 +2,22 @@
 
 import AntDChatBot from "@/components/AI/AntDChatBot";
 import PageContainer from "@/components/PageContainer";
+import { Grid } from "antd";
 import Head from "next/head";
 
 const ChatAI = () => {
+  const breakPoint = Grid.useBreakpoint();
+
   return (
     <>
       <Head>
         <title>Chat AI</title>
       </Head>
       <PageContainer
+        childrenContentStyle={{
+          padding: breakPoint?.xs ? 0 : null,
+        }}
         title="Chat AI"
-        content="Artificial Intelligence untuk Pelayanan yang Responsif, Inovatif, Modern dan Akuntabel"
       >
         <AntDChatBot />
       </PageContainer>
