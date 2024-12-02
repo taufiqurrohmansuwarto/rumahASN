b751df286efc88881e86837ab856345f748b2595
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-12-02 11:46:29 +0700

    last commit

--- controller/test-generate.controller.js
+++ controller/test-generate.controller.js
@@ -1,25 +1,47 @@
-import { generateDocument } from "@/utils/toolservice";
+import { cachedDataPegawai } from "@/utils/cached-data";
+import { createRedisInstance } from "@/utils/redis";
 
 export const testGenerate = async (req, res) => {
   try {
-    const data = {
-      peserta: [
-        {
-          nama: "iput",
-          jabatan: "Kepala Dinas",
-        },
-        {
-          nama: "dodik",
-          jabatan: "Kepala Dinas",
-        },
-      ],
-      lokasi: "Jakarta",
-      tanggal: "2024-01-01",
-      nomor: "1234567890",
-      tujuan: "Tujuan",
-    };
-    const result = await generateDocument(data, req.mc);
-    return res.json({ url: result });
+    const { fetcher: masterFetcher, siasnRequest: siasnFetcher } = req;
+    const employeeNumber = req.user.employee_number;
+
+    const redis = await createRedisInstance();
+
+    // remove all redis data
+    await redis.del(`data-pegawai-${employeeNumber}`);
+
+    const dataPegawai = await redis.call(
+      "JSON.GET",
+      `data-pegawai-${employeeNumber}`,
+      "$"
+    );
+
+    console.log(dataPegawai);
+
+    if (!dataPegawai) {
+      const result = await cachedDataPegawai(
+        masterFetcher,
+        siasnFetcher,
+        employeeNumber
+      );
+
+      // caching 1 hour
+      await redis
+        .pipeline()
+        .call(
+          "JSON.SET",
+          `data-pegawai-${employeeNumber}`,
+          "$",
+          JSON.stringify(result)
+        )
+        // 15 second
+        .expire(15)
+        .exec();
+      res.json(result);
+    } else {
+      res.json(JSON.parse(dataPegawai));
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
--- pages/api/testing.js
+++ pages/api/testing.js
@@ -1,8 +1,9 @@
 import { testGenerate } from "@/controller/test-generate.controller";
 import { createRouter } from "next-connect";
 import auth from "@/middleware/auth.middleware";
+import { siasnMiddleware } from "@/middleware/siasn.middleware";
 const router = createRouter();
 
-router.use(auth).get(testGenerate);
+router.use(auth).use(siasnMiddleware).get(testGenerate);
 
 export default router.handler();
