e257a8a54932cd0a5de127906bf5039e00a699cc
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-12-21 11:56:10 +0700

    fix tickets

--- pages/api/tickets/index.js
+++ pages/api/tickets/index.js
@@ -1,8 +1,9 @@
 const { publishedTickets } = require("@/controller/tickets-props.controller");
-const { default: auth } = require("@/middleware/auth.middleware");
-const { createRouter } = require("next-connect");
+import auth from "@/middleware/auth.middleware";
+import { createRouter } from "next-connect";
 
 const router = createRouter();
+
 router.use(auth).get(publishedTickets);
 
 export default router.handler({});
