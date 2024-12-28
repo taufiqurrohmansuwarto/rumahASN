013c4ae8b2e8e899e14aabe4c6dd0c5803a569a2
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-12-28 23:47:10 +0700

    last commit

--- pages/api/tool-services/get-data-utama-siasn.js
+++ pages/api/tool-services/get-data-utama-siasn.js
@@ -1,9 +1,9 @@
 import { getDataUtamaSiasn } from "@/controller/tool-services.controller";
+import { checkApiKey, checkRole } from "@/middleware/tool-services.middleware";
 import { createRouter } from "next-connect";
-import { checkRole } from "@/middleware/tool-services.middleware";
 const router = createRouter();
 
-router.use(checkRole).post(getDataUtamaSiasn);
+router.use(checkApiKey).use(checkRole).post(getDataUtamaSiasn);
 
 export default router.handler({
   onError: (err, req, res, next) => {
--- src/components/Utils/UnorMaster.js
+++ src/components/Utils/UnorMaster.js
@@ -2,7 +2,7 @@ import { getOpdFasilitator } from "@/services/master.services";
 import { useQuery } from "@tanstack/react-query";
 import { Form, TreeSelect } from "antd";
 
-function FormUnorMaster({ name, type }) {
+function FormUnorMaster({ name, type, required = false }) {
   const { data, isLoading } = useQuery(
     ["unor-fasilitator-master"],
     () => getOpdFasilitator(),
@@ -12,7 +12,7 @@ function FormUnorMaster({ name, type }) {
   return (
     <>
       {data && (
-        <Form.Item name={name} label="Pilih Unor">
+        <Form.Item name={name} label="Pilih Unor" required={required}>
           <TreeSelect showSearch treeNodeFilterProp="title" treeData={data} />
         </Form.Item>
       )}
