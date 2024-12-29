5da283be46686a9ef35d83575b2b17664aff93db
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2024-12-29 22:46:56 +0700

    fix chat-ai

--- pages/api/tool-services/generate-document-spt.js
+++ pages/api/tool-services/generate-document-spt.js
@@ -1,9 +1,9 @@
 import { generateDocumentSpt } from "@/controller/tool-services.controller";
+import { checkApiKey, checkRole } from "@/middleware/tool-services.middleware";
 import { createRouter } from "next-connect";
-import { checkRole } from "@/middleware/tool-services.middleware";
 const router = createRouter();
 
-router.use(checkRole).post(generateDocumentSpt);
+router.use(checkApiKey).use(checkRole).post(generateDocumentSpt);
 
 export default router.handler({
   onError: (err, req, res, next) => {
