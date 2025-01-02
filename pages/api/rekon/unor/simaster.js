8390cae6fde452717eaffd75c3b78d5740805e42
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-01-02 08:41:07 +0700

    fix something

--- pages/api/rekon/unor/simaster.js
+++ pages/api/rekon/unor/simaster.js
@@ -1,9 +1,9 @@
 import { createRouter } from "next-connect";
 import { getUnorSimaster } from "@/controller/rekon.controller";
-import auth from "@/middleware/auth";
+import auth from "@/middleware/auth.middleware";
 
 const router = createRouter();
 
-router.use().get(getUnorSimaster);
+router.use(auth).get(getUnorSimaster);
 
 export default router.handler();
