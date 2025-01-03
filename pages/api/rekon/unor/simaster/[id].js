372092991ffb6789afebab0310a56afb5907ef3b
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-01-03 12:17:18 +0700

    last commit

---
+++ pages/api/rekon/unor/simaster/[id].js
@@ -0,0 +1,10 @@
+import { getDetailUnorSimaster } from "@/controller/rekon.controller";
+import adminFasilitatorMiddleware from "@/middleware/admin-fasilitator.middleware";
+import auth from "@/middleware/auth.middleware";
+import { createRouter } from "next-connect";
+
+const router = createRouter();
+
+router.use(auth).use(adminFasilitatorMiddleware).get(getDetailUnorSimaster);
+
+export default router.handler();
