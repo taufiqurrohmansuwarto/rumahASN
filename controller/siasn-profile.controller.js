49b620eea2b3dd55b0dbb82467ce67a4bdf812bc
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-02-15 20:05:09 +0700

    tambah edit foto

--- controller/siasn-profile.controller.js
+++ controller/siasn-profile.controller.js
@@ -5,14 +5,19 @@ const {
   pasangan,
   dataUtama,
 } = require("@/utils/siasn-utils");
+const SiasnEmployees = require("@/models/siasn-employees.model");
 
 module.exports.fotoPns = async (req, res) => {
   try {
     const { siasnRequest: fetcher } = req;
     const { employee_number: nip } = req?.user;
-    const hasil = await dataUtama(fetcher, nip);
+    // const hasil = await dataUtama(fetcher, nip);
+    const hasil = await SiasnEmployees.query()
+      .where("nip_baru", nip)
+      .first()
+      .select("pns_id");
 
-    const result = await foto(fetcher, hasil?.id);
+    const result = await foto(fetcher, hasil?.pns_id);
     const data = result?.data;
 
     // blob to base64
@@ -24,6 +29,32 @@ module.exports.fotoPns = async (req, res) => {
 
     res.json(payload);
     // custom headers for image png
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+module.exports.fotoByNip = async (req, res) => {
+  try {
+    const { siasnRequest: fetcher } = req;
+    const { nip } = req?.query;
+    const hasil = await SiasnEmployees.query()
+      .where("nip_baru", nip)
+      .first()
+      .select("pns_id");
+
+    const result = await foto(fetcher, hasil?.pns_id);
+    const data = result?.data;
+
+    // blob to base64
+    const base64 = Buffer.from(data, "binary").toString("base64");
+    const photos = `data:image/png;base64,${base64}`;
+    const payload = {
+      data: photos,
+    };
+
+    res.json(payload);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
---
+++ pages/api/siasn/ws/admin/[nip]/foto.js
@@ -0,0 +1,14 @@
+import { fotoByNip } from "@/controller/siasn-profile.controller";
+import adminFasilitatorMiddleware from "@/middleware/admin-fasilitator.middleware";
+import auth from "@/middleware/auth.middleware";
+import { siasnMiddleware } from "@/middleware/siasn.middleware";
+import { createRouter } from "next-connect";
+const router = createRouter();
+
+router
+  .use(auth)
+  .use(adminFasilitatorMiddleware)
+  .use(siasnMiddleware)
+  .get(fotoByNip);
+
+export default router.handler();
