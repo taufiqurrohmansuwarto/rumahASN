73e97908e39aacac0ca19c83ed80e734ab27ef57
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-03-01 22:27:38 +0700

    add some latest

---
+++ migrations/20250301031604_add unique to column nip and updated in table siasn_ipasn.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("siasn_ipasn", function (table) {
+    table.unique(["nip", "updated"]);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("siasn_ipasn", function (table) {
+    table.dropUnique(["nip", "updated"]);
+  });
+};
