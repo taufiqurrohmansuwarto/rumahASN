eaaf87d83b8b16ada129d85b1ad27f06580e6f89
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-03-06 11:59:42 +0700

    add to user to dashboard

--- pages/api/rekon/ipasn/dashboard/index.js
+++ pages/api/rekon/ipasn/dashboard/index.js
@@ -1,10 +1,10 @@
-import adminMiddleware from "@/middleware/admin.middleware";
-import { createRouter } from "next-connect";
 import { dashboardSiasnIPASN } from "@/controller/rekon/rekon-ipasn.controller";
+import adminFasilitatorMiddleware from "@/middleware/admin-fasilitator.middleware";
 import auth from "@/middleware/auth.middleware";
+import { createRouter } from "next-connect";
 
 const router = createRouter();
 
-router.use(auth).use(adminMiddleware).get(dashboardSiasnIPASN);
+router.use(auth).use(adminFasilitatorMiddleware).get(dashboardSiasnIPASN);
 
 export default router.handler({});
