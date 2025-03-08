1b7e57c6badc47ac3a216d46b0c7821bbc048b25
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-03-08 10:28:57 +0700

    add ipasn detail

--- src/components/Rekon/RekonIPASNDetail.js
+++ src/components/Rekon/RekonIPASNDetail.js
@@ -1,10 +1,26 @@
-import { Card, Col, Row, Statistic } from "antd";
+import {
+  Avatar,
+  Card,
+  Col,
+  Input,
+  Row,
+  Space,
+  Statistic,
+  Table,
+  Tag,
+  Typography,
+} from "antd";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/router";
-import { getRekonIPASNDashboard } from "@/services/rekon.services";
+import {
+  getEmployeeIPASN,
+  getRekonIPASNDashboard,
+  getUnorSimaster,
+} from "@/services/rekon.services";
 import { Form, TreeSelect, Button } from "antd";
-import { FileExcelOutlined } from "@ant-design/icons";
+import { FileExcelOutlined, SearchOutlined } from "@ant-design/icons";
 import XLSX from "xlsx";
+import { clearQuery } from "@/utils/client-utils";
 
 const DetailIPASN = () => {
   const router = useRouter();
@@ -108,12 +124,196 @@ const DetailIPASN = () => {
   );
 };
 
+const EmployeeIPASN = () => {
+  const router = useRouter();
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: ["rekon-ipasn-employees", router?.query],
+    queryFn: () => getEmployeeIPASN(router?.query),
+    keepPreviousData: true,
+    refetchOnWindowFocus: false,
+  });
+
+  const handleTableChange = (pagination, filters, sorter) => {
+    const query = clearQuery({
+      ...router?.query,
+      page: pagination?.current || router?.query?.page, // Gunakan current page dari pagination
+      perPage: pagination?.pageSize,
+      sort: sorter?.field,
+      order: sorter?.order,
+    });
+
+    router.push({
+      pathname: "/rekon/dashboard/ipasn",
+      query,
+    });
+  };
+
+  const handleSearch = (value) => {
+    const query = clearQuery({
+      ...router?.query,
+      search: value || undefined,
+      page: 1, // Reset ke halaman pertama saat pencarian
+    });
+
+    router.push({
+      pathname: "/rekon/dashboard/ipasn",
+      query,
+    });
+  };
+
+  const renderSearchFilter = ({
+    setSelectedKeys,
+    selectedKeys,
+    confirm,
+    clearFilters,
+  }) => (
+    <div style={{ padding: 8 }}>
+      <Input
+        placeholder="Cari nama"
+        value={selectedKeys[0] || router?.query?.search}
+        onChange={(e) =>
+          setSelectedKeys(e.target.value ? [e.target.value] : [])
+        }
+        onPressEnter={() => {
+          confirm();
+          handleSearch(selectedKeys[0]);
+        }}
+        style={{ width: 300, marginBottom: 8, display: "block" }}
+      />
+      <Space>
+        <Button
+          type="primary"
+          onClick={() => {
+            confirm();
+            handleSearch(selectedKeys[0]);
+          }}
+          icon={<SearchOutlined />}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Cari
+        </Button>
+        <Button
+          onClick={() => {
+            clearFilters();
+            setSelectedKeys([]); // Pastikan state lokal juga direset
+            handleSearch(undefined); // Kirim undefined untuk menghapus parameter search
+          }}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Reset
+        </Button>
+      </Space>
+    </div>
+  );
+
+  const renderValue = (value, max) => `${value || 0}/${max}`;
+
+  const columns = [
+    {
+      title: "Foto",
+      dataIndex: "foto_master",
+      render: (text) => <Avatar size={90} shape="square" src={text} />,
+    },
+    {
+      title: "Nama",
+      key: "nama_master",
+      width: 250,
+      render: (_, record) => (
+        <Space direction="vertical">
+          <Typography.Text>{record?.nama_master}</Typography.Text>
+          <Typography.Text>{record?.nip}</Typography.Text>
+          <Typography.Text strong>{record?.jabatan_master}</Typography.Text>
+        </Space>
+      ),
+      sorter: true,
+      filterSearch: true,
+      filterDropdown: renderSearchFilter,
+      filterIcon: (filtered) => (
+        <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+      ),
+    },
+    {
+      title: "Perangkat Daerah",
+      dataIndex: "opd_master",
+      sorter: true,
+      render: (text) => text || "-",
+    },
+    {
+      title: "Status Kepegawaian",
+      dataIndex: "status_master",
+    },
+    {
+      title: "Kualifikasi",
+      dataIndex: "kualifikasi",
+      sorter: true,
+      render: (value) => renderValue(value, 25),
+    },
+    {
+      title: "Kompetensi",
+      dataIndex: "kompetensi",
+      sorter: true,
+      render: (value) => renderValue(value, 35),
+    },
+    {
+      title: "Kinerja",
+      dataIndex: "kinerja",
+      sorter: true,
+      render: (value) => renderValue(value, 30),
+    },
+    {
+      title: "Disiplin",
+      dataIndex: "disiplin",
+      sorter: true,
+      render: (value) => renderValue(value, 5),
+    },
+    {
+      title: "Total",
+      dataIndex: "total",
+      sorter: true,
+      render: (value) => renderValue(value, 100),
+    },
+
+    {
+      title: "Aksi",
+      key: "aksi",
+      render: () => (
+        <a>
+          <SearchOutlined />
+        </a>
+      ),
+    },
+  ];
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={data?.data}
+      loading={isLoading || isFetching}
+      rowKey="nip"
+      onChange={handleTableChange}
+      pagination={{
+        position: ["bottomRight", "topRight"],
+        showSizeChanger: false,
+        total: data?.totalData,
+        showTotal: (total, range) =>
+          `${range[0]}-${range[1]} of ${total} items`,
+        pageSize: data?.perPage,
+        current: data?.page,
+      }}
+    />
+  );
+};
+
 const RekonIPASNDetail = () => {
   const router = useRouter();
   const { data, isLoading } = useQuery(
     ["rekon-unor-simaster"],
     () => getUnorSimaster(),
-    {}
+    {
+      refetchOnWindowFocus: false,
+    }
   );
 
   const { mutateAsync: rekonIpasn, isLoading: isRekonIpasnLoading } =
@@ -162,7 +362,14 @@ const RekonIPASNDetail = () => {
         </Button>
       </Col>
       <Col md={24} xs={24}>
-        <DetailIPASN />
+        <Card>
+          <DetailIPASN />
+        </Card>
+      </Col>
+      <Col md={24} xs={24}>
+        <Card>
+          <EmployeeIPASN />
+        </Card>
       </Col>
     </Row>
   );
