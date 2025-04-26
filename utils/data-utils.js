6727456c91713554ac9533401f0a95b18f858eae
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-04-26 18:52:17 +0700

    add more commit

--- utils/data-utils.js
+++ utils/data-utils.js
@@ -52,3 +52,619 @@ export const statusHidup = [
   { id: "1", label: "Hidup" },
   { id: "0", label: "Meninggal" },
 ];
+
+export const imutKategori = [
+  {
+    id_kategori: "26672431-b529-410b-a204-5746770346b5",
+    kategori: "Pemberhentian",
+  },
+  {
+    id_kategori: "7f408ee0-e382-4db8-af85-6f0ba05f1b03",
+    kategori: "Evaluasi Kinerja",
+  },
+  {
+    id_kategori: "b16d8526-47ba-472d-bdc4-a7b7dab34938",
+    kategori: "Mutasi",
+  },
+  {
+    id_kategori: "d4a1f100-32f0-43dc-88fa-b2f27d7c65e6",
+    kategori: "Pengangkatan",
+  },
+  {
+    id_kategori: "da2f5339-bcb6-4d1f-b7a9-1678a708fd8e",
+    kategori: "Pengukuhan",
+  },
+  {
+    id_kategori: "e4e3e89c-e0c3-4b14-8dab-ebfe3f260f53",
+    kategori: "Promosi",
+  },
+];
+
+export const imutJenis = [
+  {
+    id: 39,
+    id_kat_detail: "d4a1f100-32f0-43dc-88fa-b2f27d7c65e6",
+    nama_kat_detail: "CPNS ke PNS",
+  },
+  {
+    id: 40,
+    id_kat_detail: "d4a1f100-32f0-43dc-88fa-b2f27d7c65e6",
+    nama_kat_detail: "Ke Jabatan Fungsional",
+  },
+  {
+    id: 41,
+    id_kat_detail: "d4a1f100-32f0-43dc-88fa-b2f27d7c65e6",
+    nama_kat_detail: "Jabatan Fungsional Ke Jabatan Manajerial",
+  },
+  {
+    id: 42,
+    id_kat_detail: "d4a1f100-32f0-43dc-88fa-b2f27d7c65e6",
+    nama_kat_detail: "Ke Jabatan Manajerial",
+  },
+  {
+    id: 43,
+    id_kat_detail: "d4a1f100-32f0-43dc-88fa-b2f27d7c65e6",
+    nama_kat_detail: "Tugas Tambahan",
+  },
+  {
+    id: 44,
+    id_kat_detail: "e4e3e89c-e0c3-4b14-8dab-ebfe3f260f53",
+    nama_kat_detail: "Kenaikan Jenjang Jabatan Fungsional",
+  },
+  {
+    id: 45,
+    id_kat_detail: "26672431-b529-410b-a204-5746770346b5",
+    nama_kat_detail: "Dari ASN",
+  },
+  {
+    id: 46,
+    id_kat_detail: "26672431-b529-410b-a204-5746770346b5",
+    nama_kat_detail: "Dari Jabatan",
+  },
+  {
+    id: 47,
+    id_kat_detail: "26672431-b529-410b-a204-5746770346b5",
+    nama_kat_detail: "Dari Tugas Tambahan",
+  },
+  {
+    id: 48,
+    id_kat_detail: "26672431-b529-410b-a204-5746770346b5",
+    nama_kat_detail: "Hukuman Disiplin",
+  },
+  {
+    id: 49,
+    id_kat_detail: "b16d8526-47ba-472d-bdc4-a7b7dab34938",
+    nama_kat_detail: "Jabatan Pimpinan Tinggi",
+  },
+  {
+    id: 50,
+    id_kat_detail: "b16d8526-47ba-472d-bdc4-a7b7dab34938",
+    nama_kat_detail: "Jabatan Administrator",
+  },
+  {
+    id: 51,
+    id_kat_detail: "b16d8526-47ba-472d-bdc4-a7b7dab34938",
+    nama_kat_detail: "Jabatan Pengawas",
+  },
+  {
+    id: 52,
+    id_kat_detail: "b16d8526-47ba-472d-bdc4-a7b7dab34938",
+    nama_kat_detail: "Kepala Puskesmas",
+  },
+  {
+    id: 53,
+    id_kat_detail: "b16d8526-47ba-472d-bdc4-a7b7dab34938",
+    nama_kat_detail: "Kepala Sekolah",
+  },
+  {
+    id: 54,
+    id_kat_detail: "b16d8526-47ba-472d-bdc4-a7b7dab34938",
+    nama_kat_detail: "Jabatan Fungsional",
+  },
+  {
+    id: 55,
+    id_kat_detail: "b16d8526-47ba-472d-bdc4-a7b7dab34938",
+    nama_kat_detail: "Khusus Jabatan Guru",
+  },
+  {
+    id: 56,
+    id_kat_detail: "b16d8526-47ba-472d-bdc4-a7b7dab34938",
+    nama_kat_detail: "Jabatan Pelaksana",
+  },
+  {
+    id: 57,
+    id_kat_detail: "b16d8526-47ba-472d-bdc4-a7b7dab34938",
+    nama_kat_detail: "PPPK",
+  },
+  {
+    id: 58,
+    id_kat_detail: "da2f5339-bcb6-4d1f-b7a9-1678a708fd8e",
+    nama_kat_detail: "Jabatan Pimpinan Tinggi",
+  },
+  {
+    id: 59,
+    id_kat_detail: "da2f5339-bcb6-4d1f-b7a9-1678a708fd8e",
+    nama_kat_detail: "Jabatan Administrator",
+  },
+  {
+    id: 60,
+    id_kat_detail: "da2f5339-bcb6-4d1f-b7a9-1678a708fd8e",
+    nama_kat_detail: "Jabatan Pengawas",
+  },
+  {
+    id: 61,
+    id_kat_detail: "da2f5339-bcb6-4d1f-b7a9-1678a708fd8e",
+    nama_kat_detail: "Jabatan Fungsional",
+  },
+  {
+    id: 62,
+    id_kat_detail: "da2f5339-bcb6-4d1f-b7a9-1678a708fd8e",
+    nama_kat_detail: "Jabatan Pelaksana",
+  },
+  {
+    id: 63,
+    id_kat_detail: "26672431-b529-410b-a204-5746770346b5",
+    nama_kat_detail: "Pemutusan Hubungan Perjanjian Kerja PPPK",
+  },
+  {
+    id: 64,
+    id_kat_detail: "7f408ee0-e382-4db8-af85-6f0ba05f1b03",
+    nama_kat_detail: "Jabatan Pimpinan Tinggi",
+  },
+];
+
+export const imutSubJenis = [
+  {
+    id: 401,
+    nama: "Pengangkatan Pertama",
+    id_kat_jenis: 40,
+  },
+  {
+    id: 402,
+    nama: "Perpindahan dari jabatan lain",
+    id_kat_jenis: 40,
+  },
+  {
+    id: 403,
+    nama: "Penyesuaian/Inpassing",
+    id_kat_jenis: 40,
+  },
+  {
+    id: 404,
+    nama: "Pengangkatan Kembali",
+    id_kat_jenis: 40,
+  },
+  {
+    id: 411,
+    nama: "Jabatan Pengawas",
+    id_kat_jenis: 41,
+  },
+  {
+    id: 412,
+    nama: "Jabatan Administrator",
+    id_kat_jenis: 41,
+  },
+  {
+    id: 421,
+    nama: "Jabatan Pimpinan Tinggi",
+    id_kat_jenis: 42,
+  },
+  {
+    id: 422,
+    nama: "Jabatan Pengawas",
+    id_kat_jenis: 42,
+  },
+  {
+    id: 423,
+    nama: "Jabatan Administrator",
+    id_kat_jenis: 42,
+  },
+  {
+    id: 431,
+    nama: "Kepala Puskesmas",
+    id_kat_jenis: 43,
+  },
+  {
+    id: 432,
+    nama: "Kepala Sanggar Kegiatan Belajar",
+    id_kat_jenis: 43,
+  },
+  {
+    id: 451,
+    nama: "Pemberhentian Dengan Hormat",
+    id_kat_jenis: 45,
+  },
+  {
+    id: 452,
+    nama: "Pemberhentian Tidak Dengan Hormat",
+    id_kat_jenis: 45,
+  },
+  {
+    id: 453,
+    nama: "Pemberhentian Sementara Dari PNS",
+    id_kat_jenis: 45,
+  },
+  {
+    id: 454,
+    nama: "CPNS Meninggal Dunia",
+    id_kat_jenis: 45,
+  },
+  {
+    id: 461,
+    nama: "Jabatan Pimpinan Tinggi",
+    id_kat_jenis: 46,
+  },
+  {
+    id: 462,
+    nama: "Jabatan Administrator",
+    id_kat_jenis: 46,
+  },
+  {
+    id: 463,
+    nama: "Jabatan Pengawas",
+    id_kat_jenis: 46,
+  },
+  {
+    id: 464,
+    nama: "Jabatan Fungsional",
+    id_kat_jenis: 46,
+  },
+  {
+    id: 471,
+    nama: "Kepala Sekolah",
+    id_kat_jenis: 47,
+  },
+  {
+    id: 472,
+    nama: "Kepala Puskesmas",
+    id_kat_jenis: 47,
+  },
+  {
+    id: 481,
+    nama: "Penurunan Jabatan Setingkat Lebih Rendah Selama 12 Bulan",
+    id_kat_jenis: 48,
+  },
+  {
+    id: 482,
+    nama: "Pembebasan dari Jabatannya Menjadi Jabatan Pelaksana Selama 12 Bulan",
+    id_kat_jenis: 48,
+  },
+  {
+    id: 483,
+    nama: "Pemberhentian Dengan Hormat Tidak Atas Permintaan Sendiri (PDHTAPS)",
+    id_kat_jenis: 48,
+  },
+  {
+    id: 501,
+    nama: "Umum",
+    id_kat_jenis: 50,
+  },
+  {
+    id: 502,
+    nama: "Khusus (DUKCAPIL)",
+    id_kat_jenis: 50,
+  },
+  {
+    id: 511,
+    nama: "Umum",
+    id_kat_jenis: 51,
+  },
+  {
+    id: 512,
+    nama: "Khusus (DUKCAPIL)",
+    id_kat_jenis: 51,
+  },
+  {
+    id: 561,
+    nama: "Unor",
+    id_kat_jenis: 56,
+  },
+  {
+    id: 562,
+    nama: "Jabatan",
+    id_kat_jenis: 56,
+  },
+  {
+    id: 563,
+    nama: "Unor dan Jabatan",
+    id_kat_jenis: 56,
+  },
+  {
+    id: 571,
+    nama: "Unor",
+    id_kat_jenis: 57,
+  },
+  {
+    id: 611,
+    nama: "Jabatan",
+    id_kat_jenis: 61,
+  },
+  {
+    id: 612,
+    nama: "Jabatan dan Unor",
+    id_kat_jenis: 61,
+  },
+  {
+    id: 613,
+    nama: "Unor",
+    id_kat_jenis: 61,
+  },
+  {
+    id: 621,
+    nama: "Jabatan",
+    id_kat_jenis: 62,
+  },
+  {
+    id: 622,
+    nama: "Jabatan dan Unor",
+    id_kat_jenis: 62,
+  },
+  {
+    id: 623,
+    nama: "Unor",
+    id_kat_jenis: 62,
+  },
+  {
+    id: 631,
+    nama: "Meninggal Dunia",
+    id_kat_jenis: 63,
+  },
+  {
+    id: 632,
+    nama: "Dengan Hormat Tidak Atas Permintaan Sendiri",
+    id_kat_jenis: 63,
+  },
+];
+
+export const imutRumpunSubJenis = [
+  {
+    id: 4021,
+    nama: "Umum",
+    id_sub_kat_jenis: 402,
+  },
+  {
+    id: 4022,
+    nama: "Khusus (Dosen)",
+    id_sub_kat_jenis: 402,
+  },
+  {
+    id: 4041,
+    nama: "Umum",
+    id_sub_kat_jenis: 404,
+  },
+  {
+    id: 4042,
+    nama: "Khusus (CLTN)",
+    id_sub_kat_jenis: 404,
+  },
+  {
+    id: 4111,
+    nama: "Umum",
+    id_sub_kat_jenis: 411,
+  },
+  {
+    id: 4112,
+    nama: "Khusus (DUKCAPIL)",
+    id_sub_kat_jenis: 411,
+  },
+  {
+    id: 4121,
+    nama: "Umum",
+    id_sub_kat_jenis: 412,
+  },
+  {
+    id: 4122,
+    nama: "Khusus (Camat, Inspektur Pembantu)",
+    id_sub_kat_jenis: 412,
+  },
+  {
+    id: 4123,
+    nama: "Khusus (DUKCAPIL)",
+    id_sub_kat_jenis: 412,
+  },
+  {
+    id: 4211,
+    nama: "Instansi Wajib Seleksi terbuka",
+    id_sub_kat_jenis: 421,
+  },
+  {
+    id: 4212,
+    nama: "Instansi Dikecualikan Dari Seleksi terbuka",
+    id_sub_kat_jenis: 421,
+  },
+  {
+    id: 4213,
+    nama: "Khusus (Sekretaris Daerah) Seleksi Terbuka",
+    id_sub_kat_jenis: 421,
+  },
+  {
+    id: 4214,
+    nama: "Khusus (KASAT POL PP) Seleksi Terbuka",
+    id_sub_kat_jenis: 421,
+  },
+  {
+    id: 4215,
+    nama: "Khusus (Sekretaris DPRD) Seleksi Terbuka",
+    id_sub_kat_jenis: 421,
+  },
+  {
+    id: 4216,
+    nama: "Khusus (Inspektur) Seleksi Terbuka",
+    id_sub_kat_jenis: 421,
+  },
+  {
+    id: 4221,
+    nama: "Umum",
+    id_sub_kat_jenis: 422,
+  },
+  {
+    id: 4222,
+    nama: "Khusus (DUKCAPIL)",
+    id_sub_kat_jenis: 422,
+  },
+  {
+    id: 4231,
+    nama: "Umum",
+    id_sub_kat_jenis: 423,
+  },
+  {
+    id: 4232,
+    nama: "Khusus (Camat, Inspektur Pembantu)",
+    id_sub_kat_jenis: 423,
+  },
+  {
+    id: 4233,
+    nama: "Khusus (DUKCAPIL)",
+    id_sub_kat_jenis: 423,
+  },
+  {
+    id: 4511,
+    nama: "Atas Permintaan Sendiri",
+    id_sub_kat_jenis: 451,
+  },
+  {
+    id: 4513,
+    nama: "Tidak Atas Permintaan Sendiri",
+    id_sub_kat_jenis: 451,
+  },
+  {
+    id: 4531,
+    nama: "Pejabat Negara",
+    id_sub_kat_jenis: 453,
+  },
+  {
+    id: 4532,
+    nama: "Komisioner / Anggota Lembaga Non-Struktural",
+    id_sub_kat_jenis: 453,
+  },
+  {
+    id: 4533,
+    nama: "Ditahan Karena Tindak Pidana",
+    id_sub_kat_jenis: 453,
+  },
+  {
+    id: 4611,
+    nama: "Mengundurkan Diri",
+    id_sub_kat_jenis: 461,
+  },
+  {
+    id: 4612,
+    nama: "Diberhentikan Sementara dari PNS",
+    id_sub_kat_jenis: 461,
+  },
+  {
+    id: 4613,
+    nama: "Menjalani Tugas Belajar Lebih Dari 6 Bulan",
+    id_sub_kat_jenis: 461,
+  },
+  {
+    id: 4614,
+    nama: "Ditugaskan Secara Penuh Diluar Jabatan",
+    id_sub_kat_jenis: 461,
+  },
+  {
+    id: 4615,
+    nama: "Terjadi Penataan Organisasi",
+    id_sub_kat_jenis: 461,
+  },
+  {
+    id: 4616,
+    nama: "Tidak Memenuhi Persyaratan Jabatan",
+    id_sub_kat_jenis: 461,
+  },
+  {
+    id: 4617,
+    nama: "Pembebasan Dari Jabatannya Menjadi Kepala Desa",
+    id_sub_kat_jenis: 461,
+  },
+  {
+    id: 4621,
+    nama: "Mengundurkan Diri",
+    id_sub_kat_jenis: 462,
+  },
+  {
+    id: 4622,
+    nama: "Menjalani Tugas Belajar Lebih Dari 6 Bulan",
+    id_sub_kat_jenis: 462,
+  },
+  {
+    id: 4623,
+    nama: "Ditugaskan Secara Penuh Diluar Jabatan",
+    id_sub_kat_jenis: 462,
+  },
+  {
+    id: 4624,
+    nama: "Terjadi Penataan Organisasi",
+    id_sub_kat_jenis: 462,
+  },
+  {
+    id: 4625,
+    nama: "Tidak Memenuhi Persyaratan Jabatan",
+    id_sub_kat_jenis: 462,
+  },
+  {
+    id: 4626,
+    nama: "Pembebasan Dari Jabatannya Menjadi Kepala Desa",
+    id_sub_kat_jenis: 462,
+  },
+  {
+    id: 4631,
+    nama: "Mengundurkan Diri",
+    id_sub_kat_jenis: 463,
+  },
+  {
+    id: 4632,
+    nama: "Menjalani Tugas Belajar Lebih Dari 6 Bulan",
+    id_sub_kat_jenis: 463,
+  },
+  {
+    id: 4633,
+    nama: "Ditugaskan Secara Penuh Diluar Jabatan",
+    id_sub_kat_jenis: 463,
+  },
+  {
+    id: 4634,
+    nama: "Terjadi Penataan Organisasi",
+    id_sub_kat_jenis: 463,
+  },
+  {
+    id: 4635,
+    nama: "Tidak Memenuhi Persyaratan Jabatan",
+    id_sub_kat_jenis: 463,
+  },
+  {
+    id: 4636,
+    nama: "Pembebasan Dari Jabatannya Menjadi Kepala Desa",
+    id_sub_kat_jenis: 463,
+  },
+  {
+    id: 4641,
+    nama: "Mengundurkan Diri",
+    id_sub_kat_jenis: 464,
+  },
+  {
+    id: 4642,
+    nama: "Menjalani Tugas Belajar Lebih Dari 6 Bulan",
+    id_sub_kat_jenis: 464,
+  },
+  {
+    id: 4643,
+    nama: "Ditugaskan Secara Penuh Diluar Jabatan",
+    id_sub_kat_jenis: 464,
+  },
+  {
+    id: 4644,
+    nama: "Terjadi Penataan Organisasi",
+    id_sub_kat_jenis: 464,
+  },
+  {
+    id: 4645,
+    nama: "Tidak Memenuhi Persyaratan Jabatan",
+    id_sub_kat_jenis: 464,
+  },
+  {
+    id: 4646,
+    nama: "Pembebasan Dari Jabatannya Menjadi Kepala Desa",
+    id_sub_kat_jenis: 464,
+  },
+];
