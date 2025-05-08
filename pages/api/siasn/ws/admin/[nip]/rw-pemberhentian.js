3cba66083d69f67fd0b6b3af72ddf88f39e8ec81
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-05-08 07:52:00 +0700

    feat penambahan tabel pemberhentian hasil integrasi siasn

--- pages/api/siasn/ws/admin/[nip]/rw-pemberhentian.js
+++ pages/api/siasn/ws/admin/[nip]/rw-pemberhentian.js
@@ -1,7 +1,6 @@
-import { siasnRwPemberhentian } from "@/controller/siasn.controller";
+import { getPemberhentianByNip } from "@/controller/rekon/rekon-pemberhentian.controller";
 import adminFasilitatorMiddleware from "@/middleware/admin-fasilitator.middleware";
 import auth from "@/middleware/auth.middleware";
-import checkEmployee from "@/middleware/check-employee.middleware";
 import { siasnMiddleware } from "@/middleware/siasn.middleware";
 import { createRouter } from "next-connect";
 const router = createRouter();
@@ -10,6 +9,6 @@ router
   .use(auth)
   .use(adminFasilitatorMiddleware)
   .use(siasnMiddleware)
-  .get(siasnRwPemberhentian);
+  .get(getPemberhentianByNip);
 
 export default router.handler();
