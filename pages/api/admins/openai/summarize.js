bb11e1d018a4f20e4027e6903f86b356e0813dbe
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-05-21 10:44:50 +0700

    feat tambahan hapus summarize dan recommendation

--- pages/api/admins/openai/summarize.js
+++ pages/api/admins/openai/summarize.js
@@ -1,10 +1,17 @@
 import adminMiddleware from "@/middleware/admin.middleware";
 import { createRouter } from "next-connect";
 import auth from "@/middleware/auth.middleware";
-import { summarizeQuestion } from "@/controller/fine-tunning.controller";
+import {
+  summarizeQuestion,
+  deleteSummary,
+} from "@/controller/fine-tunning.controller";
 
 const router = createRouter();
 
-router.use(auth).use(adminMiddleware).post(summarizeQuestion);
+router
+  .use(auth)
+  .use(adminMiddleware)
+  .post(summarizeQuestion)
+  .put(deleteSummary);
 
 export default router.handler();
