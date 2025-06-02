7301cace459e9ea904eb452ff9ebd08f0e25fab0
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-06-02 18:40:04 +0700

    fix page container in bank jatim

--- pages/layanan-keuangan/bank-jatim/produk/kpr/index.js
+++ pages/layanan-keuangan/bank-jatim/produk/kpr/index.js
@@ -3,9 +3,11 @@ import PageContainer from "@/components/PageContainer";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import BankJatimDetailLayananKPR from "@/components/LayananKeuangan/DetailLayanan/BankJatimDetailLayananKPR";
+import { Grid } from "antd";
 
 const KPR = () => {
   const router = useRouter();
+  const breakPoint = Grid.useBreakpoint();
 
   const handleBack = () => {
     router.back();
@@ -20,6 +22,9 @@ const KPR = () => {
         title="Layanan Keuangan"
         subTitle="Bank Jatim - KPR"
         onBack={handleBack}
+        childrenContentStyle={{
+          padding: breakPoint.xs ? null : 0,
+        }}
       >
         <BankJatimDetailLayananKPR />
       </PageContainer>
--- pages/layanan-keuangan/bank-jatim/produk/multiguna/index.js
+++ pages/layanan-keuangan/bank-jatim/produk/multiguna/index.js
@@ -3,9 +3,11 @@ import LayananKeuanganLayout from "@/components/LayananKeuangan/LayananKeuanganL
 import PageContainer from "@/components/PageContainer";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { Grid } from "antd";
 
 const Multiguna = () => {
   const router = useRouter();
+  const breakPoint = Grid.useBreakpoint();
 
   const handleBack = () => {
     router.back();
@@ -20,6 +22,9 @@ const Multiguna = () => {
         title="Layanan Keuangan"
         subTitle="Bank Jatim - Multiguna"
         onBack={handleBack}
+        childrenContentStyle={{
+          padding: breakPoint.xs ? null : 0,
+        }}
       >
         <BankJatimDetailLayananMultiguna />
       </PageContainer>
