c69f9b7af27acfaa4d43ced8cf45e027d1b37dfc
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-07-14 10:33:45 +0700

    last commit

--- pages/api/admins/siasn/usulan-peremajaan/index.js
+++ pages/api/admins/siasn/usulan-peremajaan/index.js
@@ -2,9 +2,15 @@ import { createRouter } from "next-connect";
 import auth from "middleware/auth.middleware";
 import adminMiddleware from "@/middleware/admin.middleware";
 import { testing } from "@/controller/siasn-instansi/token.controller";
+import { siasnMiddleware } from "@/middleware/siasn.middleware";
 
 const router = createRouter();
 
-router.use(auth).use(adminMiddleware).get(testing).post(createUsulanPeremajaan);
+router
+  .use(auth)
+  .use(siasnMiddleware)
+  .use(adminMiddleware)
+  .get(testing)
+  .post(createUsulanPeremajaan);
 
 export default router.handler({});
