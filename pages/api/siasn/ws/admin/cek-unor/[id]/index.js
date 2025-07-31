d82826ed3861a5293b190c473015718e1eea5c7e
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-07-31 14:20:24 +0700

    fix(ws unor):perbaikan role untuk web service unor siasn ASN

--- pages/api/siasn/ws/admin/cek-unor/[id]/index.js
+++ pages/api/siasn/ws/admin/cek-unor/[id]/index.js
@@ -1,9 +1,9 @@
 import { cekUnor } from "@/controller/disparitas.controller";
-import adminMiddleware from "@/middleware/admin.middleware";
+import adminFasilitatorMiddleware from "@/middleware/admin-fasilitator.middleware";
 import auth from "@/middleware/auth.middleware";
 import { createRouter } from "next-connect";
 const router = createRouter();
 
-router.use(auth).use(adminMiddleware).get(cekUnor);
+router.use(auth).use(adminFasilitatorMiddleware).get(cekUnor);
 
 export default router.handler();
