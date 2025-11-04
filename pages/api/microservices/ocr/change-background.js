c7af6b9488d6260d4e4912f0aa404c596a99c167
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-11-04 11:21:21 +0700

    fix(ocr foto): mengganti endpoint

--- pages/api/microservices/ocr/change-background.js
+++ pages/api/microservices/ocr/change-background.js
@@ -1,8 +1,12 @@
 import adminFasilitatorAsnMiddleware from "@/middleware/admin-fasilitator-asn.middleware";
 import auth from "@/middleware/auth.middleware";
 import { createRouter } from "next-connect";
+import { changeBackgroundController } from "@/controller/microservices/ocr.controller";
 const router = createRouter();
 
-router.use(auth).use(adminFasilitatorAsnMiddleware).post();
+router
+  .use(auth)
+  .use(adminFasilitatorAsnMiddleware)
+  .post(changeBackgroundController);
 
 export default router.handler();
--- pages/api/microservices/ocr/remove-background.js
+++ pages/api/microservices/ocr/remove-background.js
@@ -1,8 +1,12 @@
 import adminFasilitatorAsnMiddleware from "@/middleware/admin-fasilitator-asn.middleware";
 import auth from "@/middleware/auth.middleware";
 import { createRouter } from "next-connect";
+import { removeBackgroundController } from "@/controller/microservices/ocr.controller";
 const router = createRouter();
 
-router.use(auth).use(adminFasilitatorAsnMiddleware).post();
+router
+  .use(auth)
+  .use(adminFasilitatorAsnMiddleware)
+  .post(removeBackgroundController);
 
 export default router.handler();
