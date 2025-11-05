e2c91fcde06b123445e5292302564235b25770d8
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2025-11-06 05:56:46 +0700

    fix(paruh waktu): perbaikan middleware

--- pages/api/siasn/ws/admin/pengadaan/paruh-waktu/employees/index.js
+++ pages/api/siasn/ws/admin/pengadaan/paruh-waktu/employees/index.js
@@ -1,5 +1,5 @@
 import { getPengadaanParuhWaktu } from "@/controller/paruh-waktu.controller";
-import adminMiddleware from "@/middleware/admin.middleware";
+import adminFasilitatorMiddleware from "@/middleware/admin-fasilitator.middleware";
 import auth from "@/middleware/auth.middleware";
 import { siasnMiddleware } from "@/middleware/siasn.middleware";
 import { createRouter } from "next-connect";
@@ -8,7 +8,7 @@ const router = createRouter();
 
 router
   .use(auth)
-  .use(adminMiddleware)
+  .use(adminFasilitatorMiddleware)
   .use(siasnMiddleware)
   .get(getPengadaanParuhWaktu);
 
